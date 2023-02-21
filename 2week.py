be5a46afc1670f4a0ac09d938deaece0b1344120
jongmung <jmp99@naver.com>
2023-02-21 23:14:16 +0900

    codingst2

--- 2week.py
+++ 2week.py
@@ -32,3 +32,26 @@ def solution(numbers, num1, num2):
 # 다른사람 풀이
 def solution(numbers, num1, num2):
     return numbers[num1:num2+1]
+
+# 옷가게 할인 받기
+# 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%,
+# 50만 원 이상 사면 20%를 할인해줍니다.
+# 구매한 옷의 가격 price가 주어질 때,
+# 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+def solution(price):
+    answer = 0
+    if price >= 100000 and price < 300000:
+        answer = price * 0.95
+    elif price >= 300000 and price < 500000:
+        answer = price * 0.9
+    elif price >= 500000:
+        answer = price * 0.8
+    else:
+        answer = price
+    return int(answer)
+# 다른사람 풀이
+def solution(price):
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)
