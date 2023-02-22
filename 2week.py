526e4fbafec5264bcc16bcf8ab7fc160df532be3
jongmung <jmp99@naver.com>
2023-02-22 19:50:11 +0900

    codingst2

--- 2week.py
+++ 2week.py
@@ -55,3 +55,71 @@ def solution(price):
     for discount_price, discount_rate in discount_rates.items():
         if price >= discount_price:
             return int(price * discount_rate)
+        
+# 피자 나눠 먹기 (3)
+# 머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다.
+# 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때,
+# n명의 사람이 최소 한 조각 이상 피자를 먹으려면
+# 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+def solution(slice, n):
+    return (n-1) // slice +1
+
+# 삼각형의 완성조건 (1)
+# 선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+#  가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+# 삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다.
+# 세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
+def solution(sides):
+    answer = 1
+    a = 0
+    b = []
+    if sides[0] > sides[1]:
+        a = sides[0]
+        b.append(sides[1])
+    else:
+        a = sides[1]
+        b.append(sides[0])
+    if a > sides[2]:
+        b.append(sides[2])
+    else:
+        b.append(a)
+        a = sides[2]
+    if a < b[0]+b[1]:
+        return answer
+    else:
+        answer = 2
+    return answer
+# 다른사람 풀이
+def solution(sides):
+    return 1 if max(sides) < (sum(sides) - max(sides)) else 2
+
+# 최댓값 만들기 (1)
+# 정수 배열 numbers가 매개변수로 주어집니다.
+# numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+def solution(numbers):
+    answer = max(numbers)
+    numbers.remove(answer)
+    answer *= max(numbers)
+    return answer
+# 다른사람 풀이
+def solution(numbers):
+    numbers.sort()
+    return numbers[-2] * numbers[-1]
+
+# 아이스 아메리카노
+# 머쓱이는 추운 날에도 아이스 아메리카노만 마십니다.
+# 아이스 아메리카노는 한잔에 5,500원입니다.
+# 머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때,
+# 머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+def solution(money):
+    answer = []
+    answer.extend([money//5500,money%5500])
+    return answer
+def solution(money):
+    return [money//5500,money%5500]
+
+# 중복된 숫자 개수
+# 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때,
+# array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+def solution(array, n):
+    return array.count(n)
