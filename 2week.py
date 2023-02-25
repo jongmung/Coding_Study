d5e32f9d5b6529c0a2e53cc83db117f4d88b2331
jongmung <jmp99@naver.com>
2023-02-25 23:56:02 +0900

    codingst2

--- 2week.py
+++ 2week.py
@@ -187,3 +187,24 @@ def solution(n):
 def solution(n):
     N=[int(i) for i in str(n)]
     return sum(N)
+
+# 숨어있는 숫자의 덧셈 (1)
+# 문자열 my_string이 매개변수로 주어집니다.
+# my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+import re
+def solution(my_string):
+    answer = re.findall(r'\d',my_string)
+    answer = [int(i) for i in answer]
+    return sum(answer)
+# 다른사람 풀이
+def solution(my_string):
+    return sum(int(i) for i in my_string if i.isdigit())
+# 2
+def solution(my_string):
+    answer = 0
+    for i in my_string:
+        try:
+            answer = answer + int(i)
+        except:
+            pass
+    return answer
