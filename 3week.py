bb593c4a367ae1f74cbc7475ff1c2ecb6d50edf1
jongmung <jmp99@naver.com>
2023-02-28 22:43:05 +0900

    codingst3

--- 3week.py
+++ 3week.py
@@ -14,9 +14,16 @@ def solution(n):
     return 1 if (n ** 0.5).is_integer() else 2
 
 # 가위바위보
+# 가위는 2 바위는 0 보는 5로 표현합니다.
+# 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때,
+# rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
 def solution(rsp):
     result = {'2':'0','0':'5','5':'2'}
     answer = ''
-    for i in rsp:
+    for i in rsp:  # 딕셔너리를 만들어서 get()을 사용하여 value 값을 찾아낸다.
         answer += result.get(i)
     return answer
+# 다른사람 풀이
+def solution(rsp):
+    d = {'0':'5','2':'0','5':'2'}
+    return ''.join(d[i] for i in rsp) # '',join을 이용하여 하나로 합쳐, dict[key]를 사용하여 value 값을 가져온다.
