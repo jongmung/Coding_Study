a1b3c249b88cd88602ab479b5684b0cca6974d00
jongmung <jmp99@naver.com>
2023-03-04 23:47:02 +0900

    codingst3

--- 3week.py
+++ 3week.py
@@ -109,7 +109,18 @@ def solution(box, n):
     return (x // n) * (y // n) * (z // n )
 
 # 가장 큰 수 찾기
-# 정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
-
-
+# 정수 배열 array가 매개변수로 주어질 때,
+# 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+def solution(array):
+    answer = []
+    a = 0
+    b = 0
+    for i in range(len(array)):
+        if array[i]>a:
+            a = array[i]
+            b = i
+    return answer.extend((a,b))
+# 다른사람 풀이
+def solution(array):
+    return [max(array), array.index(max(array))]
 
