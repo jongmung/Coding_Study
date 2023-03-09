96c8f8c3f109e445906c68e5c43d5c8ca9d1def4
jongmung <jmp99@naver.com>
2023-03-09 23:48:12 +0900

    codingst3

--- 3week.py
+++ 3week.py
@@ -184,3 +184,14 @@ def solution(num, k):
 def solution(my_string):
     return ''.join(dict.fromkeys(my_string))
 
+# 문자열 정렬하기 (2)
+# 영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때,
+# my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        answer += i.lower()
+    return ''.join(sorted(answer))
+# 다른사람 풀이
+def solution(my_string):
+    return ''.join(sorted(my_string.lower()))
