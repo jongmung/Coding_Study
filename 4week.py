549d7860b299945e3b8e679db10517bde53a9451
jongmung <jmp99@naver.com>
2023-03-13 14:41:33 +0900

    codingst4

--- 4week.py
+++ 4week.py
@@ -31,3 +31,11 @@ def solution(i, j, k):
         answer += Counter(str(a))[str(k)]
     return answer
 
+# 7의 개수
+# 머쓱이는 행운의 숫자 7을 가장 좋아합니다.
+# 정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+def solution(array):
+    answer = ""
+    for i in array:
+        answer+=str(i)
+    return answer.count("7")
