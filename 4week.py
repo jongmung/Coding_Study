0c20b2247133a67ea0b5102907e19a565ceca2e9
jongmung <jmp99@naver.com>
2023-03-15 23:58:51 +0900

    codingst4

--- 4week.py
+++ 4week.py
@@ -98,3 +98,12 @@ def solution(bin1, bin2):
     answer = bin(int(bin1,2) + int(bin2,2))
     return answer[2:]
 
+# 문자열 계산하기
+# my_string은 "3 + 5"처럼 문자열로 된 수식입니다.
+# 문자열 my_string이 매개변수로 주어질 때, 수식을 계산한 값을 return 하는 solution 함수를 완성해주세요.
+def solution(my_string):
+    return eval(my_string)
+# 다른사람 풀이
+def solution(my_string):
+    return sum(int(i) for i in my_string.replace(' - ', ' + -').split(' + '))
+
