3f2ebd8ffb78e91e7336fbc653ed367c7854da02
jongmung <jmp99@naver.com>
2023-03-15 23:50:21 +0900

    codingst4

--- 4week.py
+++ 4week.py
@@ -90,3 +90,11 @@ def solution(s):
         except:
             answer.pop()      
     return sum(answer)
+
+# 이진수 더하기
+# 이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때,
+# 두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
+def solution(bin1, bin2):
+    answer = bin(int(bin1,2) + int(bin2,2))
+    return answer[2:]
+
