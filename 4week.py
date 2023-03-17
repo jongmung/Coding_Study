8a19f0985484b023d21504447ec1152fe623df79
jongmung <jmp99@naver.com>
2023-03-18 00:05:26 +0900

    codingst4

--- 4week.py
+++ 4week.py
@@ -133,3 +133,17 @@ def solution(chicken):
 def solution(chicken):
     answer = (max(chicken,1)-1)//9
     return answer
+
+# 등수 매기기
+# 영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다.
+# 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때,
+# 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+def solution(score):
+    answer = []
+    li = []
+    for i in score:
+        li.append(sum(i)/len(i))
+    sort_arr = sorted(li,reverse = True)
+    for i in li:
+        answer.append(sort_arr.index(i)+1)
+    return answer
