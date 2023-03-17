4c7979f2f49642bb5501a03bfd6105b8d2a75c02
jongmung <jmp99@naver.com>
2023-03-17 23:55:45 +0900

    codingst4

--- 4week.py
+++ 4week.py
@@ -112,3 +112,24 @@ def solution(my_string):
 # my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
 def solution(my_str, n):
     return [my_str[i:i+n] for i in range(0, len(my_str),n)]
+
+# 치킨쿠폰
+# 프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다.
+# 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고,
+# 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 
+# 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+def solution(chicken):
+    answer = 0
+    while chicken >= 10:
+        div = chicken // 10
+        mod = chicken % 10
+        answer += div
+        chicken = div+mod
+    return answer
+# 다른사람 풀이
+def solution(chicken):
+    return int(chicken*0.11111111111) 
+
+def solution(chicken):
+    answer = (max(chicken,1)-1)//9
+    return answer
