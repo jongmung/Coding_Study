e010b4feadcba50b8cbcb39410c5f36af82ecdda
jongmung <jmp99@naver.com>
2023-03-19 23:55:42 +0900

    codingst4

--- 4week.py
+++ 4week.py
@@ -154,3 +154,15 @@ def solution(score):
 def solution(M, N):
     answer = (M*N)-1
     return answer
+
+# 직사각형 넓이 구하기
+# 2차원 좌표 평면에 변이 축과 평행한 직사각형이 있습니다.
+# 직사각형 네 꼭짓점의 좌표
+# [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]가
+# 담겨있는 배열 dots가 매개변수로 주어질 때, 직사각형의 넓이를 return 하도록 solution 함수를 완성해보세요.
+def solution(dots):
+    w = max(dots)[0] - min(dots)[0]
+    h = max(dots)[1] - min(dots)[1]
+    area = w*h
+    return area
+
