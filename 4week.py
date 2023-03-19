74c925cc8dd9f77e16755202c049cc7c6a7343bd
jongmung <jmp99@naver.com>
2023-03-19 23:42:53 +0900

    codingst4

--- 4week.py
+++ 4week.py
@@ -147,3 +147,10 @@ def solution(score):
     for i in li:
         answer.append(sort_arr.index(i)+1)
     return answer
+
+# 종이 자르기
+# 정수 M, N이 매개변수로 주어질 때,
+# M x N 크기의 종이를 최소로 가위질 해야하는 횟수를 return 하도록 solution 함수를 완성해보세요.
+def solution(M, N):
+    answer = (M*N)-1
+    return answer
