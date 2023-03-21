2b6fe6c08b1560093830a9608ab7a183999c6833
jongmung <jmp99@naver.com>
2023-03-21 23:58:12 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -17,3 +17,12 @@ def solution(a, b):
 # 다른사람 풀이
 def solution(a, b):
     return 1 if a/b * 1000 % 1 == 0 else 2
+
+# 연속된 수의 합
+# 연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다.
+# 두 정수 num과 total이 주어집니다. 연속된 수 num개를 더한 값이 total이 될 때,
+# 정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해보세요.
+def solution(num, total):
+    average = total // num
+    return [i for i in range(average - (num-1)//2, average + (num + 2)//2)]
+
