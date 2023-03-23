8af742314ff99b25cf8b24ba66a29c1a8cc6716c
jongmung <jmp99@naver.com>
2023-03-23 22:48:34 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -65,3 +65,14 @@ def solution(hp):
 # 다른사람 풀이
 def solution(hp):    
     return hp // 5 + (hp % 5 // 3) + ((hp % 5) % 3)
+
+# 진료 순서 정하기
+# 외과의사 머쓱이는 응급실에 온 환자의 응급도를 기준으로 진료 순서를 정하려고 합니다.
+# 정수 배열 emergency가 매개변수로 주어질 때 응급도가 높은 순서대로
+# 진료 순서를 정한 배열을 return하도록 solution 함수를 완성해주세요.
+def solution(emergency):
+    answer = []
+    a = sorted(emergency, reverse = True)
+    for i in emergency:
+        answer.append(a.index(i)+1)
+    return answer
