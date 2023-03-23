d90f7d809df1a1f52c6a5dc71cc53beea8fbb2b5
jongmung <jmp99@naver.com>
2023-03-23 22:48:58 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -76,3 +76,6 @@ def solution(emergency):
     for i in emergency:
         answer.append(a.index(i)+1)
     return answer
+# 다른사람 풀이
+def solution(emergency):
+    return [sorted(emergency, reverse=True).index(e) + 1 for e in emergency]
