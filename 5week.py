306d203131106dbfb02ec224e6dc17ab8b1040d4
jongmung <jmp99@naver.com>
2023-03-24 23:51:00 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -79,3 +79,18 @@ def solution(emergency):
 # 다른사람 풀이
 def solution(emergency):
     return [sorted(emergency, reverse=True).index(e) + 1 for e in emergency]
+
+# 영어가 싫어요
+# 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다.
+# 문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+def solution(numbers):
+    answer = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for index,num in enumerate(answer):            # enumerate()함수는 기본적으로 인덱스와 원소로 이루어진 튜플을 만들어준다.
+        numbers = numbers.replace(num,str(index))  # for i,letter in enumerate(['A','B','C']):
+    return int(numbers)                            # print(i,letter) -> 0 A , 1 B , 2 C
+# 다른사람 풀이
+def solution(numbers):
+    for num, eng in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(eng, str(num))
+    return int(numbers)
+
