45bd007b9395d90bbf38e45450c3bb8f586f88f5
jongmung <jmp99@naver.com>
2023-03-28 23:34:03 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -128,5 +128,15 @@ def solution(letter):
         '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
         '-.--':'y','--..':'z'
     }
-
     return ''.join([morse[i] for i in letter.split(' ')])
+
+# 최빈값 구하기
+# 최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다.
+# 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요.
+# 최빈값이 여러 개면 -1을 return 합니다.
+def solution(array):
+    while len(array) != 0:
+        for i, a in enumerate(set(array)):
+            array.remove(a)
+        if i == 0: return a
+    return -1
