cc6b88a0c6258836987954add5e8538d77925879
jongmung <jmp99@naver.com>
2023-03-30 23:48:28 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -148,3 +148,14 @@ def solution(array):
 # 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
 def solution(sides):
     return (sorted(sides)[0] * 2) - 1
+
+# 외계어 사전
+# 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다.
+# spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1,
+# 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
+def solution(spell, dic):
+    spell = set(spell) 
+    for i in dic:
+        if spell.issubset(set(i)):
+            return 1 
+    return 2
