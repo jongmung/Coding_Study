357bc684b6c40d6a2607299415c559eb6fb7ec60
jongmung <jmp99@naver.com>
2023-04-02 22:54:44 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -180,3 +180,19 @@ def solution(array, n):
     return answer
 # 다른사람 풀이
 solution=lambda a,n:sorted(a,key=lambda x:(abs(x-n),x))[0]
+
+# 2차원으로 만들기
+# 정수 배열 num_list와 정수 n이 매개변수로 주어집니다.
+# num_list를 다음 설명과 같이 2차원 배열로 바꿔 return하도록 solution 함수를 완성해주세요.
+# num_list가 [1, 2, 3, 4, 5, 6, 7, 8] 로 길이가 8이고
+# n이 2이므로 num_list를 2 * 4 배열로 다음과 같이 변경합니다.
+# 2차원으로 바꿀 때에는 num_list의 원소들을 앞에서부터 n개씩 나눠 2차원 배열로 변경합니다.
+import numpy as np
+def solution(num_list, n):
+    return (np.array(num_list).reshape((len(num_list) // n ,n))).tolist()
+# 다른사람 풀이
+def solution(num_list, n):
+    answer = []
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i+n])
+    return answer
