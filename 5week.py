a1823d9cf7871eb2fe7b4f9edaff950e7c9544e5
jongmung <jmp99@naver.com>
2023-04-04 22:49:37 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -213,3 +213,19 @@ def solution(n):
         while answer%3 == 0 or '3' in str(answer):
             answer += 1
     return answer
+
+# 문자열 밀기
+# 문자열 "hello"에서 각 문자를 오른쪽으로 한 칸씩 밀고 마지막 문자는 맨 앞으로 이동시키면 "ohell"이 됩니다.
+# 이것을 문자열을 민다고 정의한다면 문자열 A와 B가 매개변수로 주어질 때,
+# A를 밀어서 B가 될 수 있다면 밀어야 하는 최소 횟수를 return하고 밀어서 B가 될 수 없으면 -1을 return 하도록 solution 함수를 완성해보세요.
+from collections import deque
+def solution(A, B):
+    Alist = deque(A)
+    Blist = deque(B)
+    for i in range(len(Alist)):
+        if Alist == Blist:
+            return i
+        Alist.rotate(1)
+    return -1
+# 다른사람 풀이
+solution=lambda a,b:(b*2).find(a)
