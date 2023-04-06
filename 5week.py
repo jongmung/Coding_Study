57577f2fd80b03a387d2ad306f05fdb3dd4ff1f6
jongmung <jmp99@naver.com>
2023-04-07 00:02:29 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -238,3 +238,33 @@ solution=lambda a,b:(b*2).find(a)
 def solution(numlist, n):
     answer = sorted(numlist, key = lambda x : (abs(x-n), -x))
     return answer
+
+# 옹알이 (1)
+# 머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다.
+# 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을
+# 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다.
+# 문자열 배열 babbling이 매개변수로 주어질 때,
+# 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+def solution(babbling):
+    answer = 0
+    for i in babbling:
+        cnt = 0
+        word = ''
+        for j in i:
+            word += j
+            if word in ['aya','ye','woo','ma']:
+                word=''
+                cnt +=1
+        if len(word) ==0 and cnt>0:
+                    answer +=1
+    return answer
+# 다른사람 풀이
+def solution(babbling):
+    c = 0
+    for b in babbling:
+        for w in [ "aya", "ye", "woo", "ma" ]:
+            if w * 2 not in b:
+                b = b.replace(w, ' ')
+        if len(b.strip()) == 0:
+            c += 1
+    return c
