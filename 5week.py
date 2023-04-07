ab72638e0c8fefafa52c893bed5033a872eb293a
jongmung <jmp99@naver.com>
2023-04-07 23:44:13 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -268,3 +268,22 @@ def solution(babbling):
         if len(b.strip()) == 0:
             c += 1
     return c
+
+# 겹치는 선분의 길이
+# 선분 3개가 평행하게 놓여 있습니다.
+# 세 선분의 시작과 끝 좌표가 [[start, end], [start, end], [start, end]]
+# 형태로 들어있는 2차원 배열 lines가 매개변수로 주어질 때,
+# 두 개 이상의 선분이 겹치는 부분의 길이를 return 하도록 solution 함수를 완성해보세요.
+def solution(lines):
+    answer = 0
+    count = [0 for _ in range(200)]
+    for line in lines:
+        for i in range(line[0], line[1]): 
+            count[i + 100] += 1
+    answer += count.count(2)
+    answer += count.count(3)
+    return answer
+# 다른사람 풀이
+def solution(lines):
+    sets = [set(range(min(l), max(l))) for l in lines]
+    return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
