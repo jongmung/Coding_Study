942955d2e92fb415857f14ed4ad8a7121bb83d0d
jongmung <jmp99@naver.com>
2023-04-08 23:34:20 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -289,11 +289,21 @@ def solution(lines):
     return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
 
 # 다음에 올 숫자 
-#
+# 등차수열 혹은 등비수열 common이 매개변수로 주어질 때,
+# 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
 def solution(common):
     answer = 0
     if common[1] - common[0] == common[2] - common[1]: 
         answer = common[-1] + (common[1] - common[0])
     elif common[1]/common[0] == common[2]/common[1]:
         answer = common[-1] * (common[1]/common[0])
+    return answer
+# 다른사람 풀이
+def solution(common):
+    answer = 0
+    a,b,c = common[:3]
+    if (b-a) == (c-b):
+        return common[-1]+(b-a)
+    else:
+        return common[-1] * (b//a)
     return answer
