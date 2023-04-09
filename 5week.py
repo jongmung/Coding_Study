054747fe382fb4ab27e11c178464f79110aafffe
jongmung <jmp99@naver.com>
2023-04-09 23:50:41 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -329,3 +329,10 @@ def solution(quiz):
             else:
                 answer.append('X')
     return answer
+# 다른사람 풀이
+def valid(equation):
+    equation = equation.replace('=', '==')
+    return eval(equation)
+
+def solution(equations):
+    return ["O" if valid(equation) else "X" for equation in equations]    
