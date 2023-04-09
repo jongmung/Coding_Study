2021eb750fb20c0c860bc3e233102311bbf1b1fc
jongmung <jmp99@naver.com>
2023-04-09 23:50:12 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -307,3 +307,25 @@ def solution(common):
     else:
         return common[-1] * (b//a)
     return answer
+
+# OX퀴즈
+# 덧셈, 뺄셈 수식들이 'X [연산자] Y = Z' 형태로 들어있는 문자열 배열 quiz가 매개변수로 주어집니다.
+# 수식이 옳다면 "O"를 틀리다면 "X"를 순서대로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+def solution(quiz):
+    answer = []
+    # [연산자]는 + 와 - 중 하나입니다.
+    for q in quiz:
+        left, right = q.split('=')
+        left = left.split()
+        print(left)
+        if left[1] == '+': # 더하기 연산
+            if int(left[0]) + int(left[2]) == int(right):
+                answer.append('O')
+            else:
+                answer.append('X')
+        elif left[1] == '-': # 빼기 연산
+            if int(left[0]) - int(left[2]) == int(right):
+                answer.append('O')
+            else:
+                answer.append('X')
+    return answer
