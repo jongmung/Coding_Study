4f76e787895b131b93935f531fd50dc779c1a28a
jongmung <jmp99@naver.com>
2023-04-10 23:47:01 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -336,3 +336,35 @@ def valid(equation):
 
 def solution(equations):
     return ["O" if valid(equation) else "X" for equation in equations]    
+
+# 다항식 더하기
+# 한 개 이상의 항의 합으로 이루어진 식을 다항식이라고 합니다.
+# 다항식을 계산할 때는 동류항끼리 계산해 정리합니다.
+# 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때,
+# 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요.
+# 같은 식이라면 가장 짧은 수식을 return 합니다.
+def solution(polynomial):
+    polynomial = polynomial.replace(' ', '').split('+')
+    
+    a, b = 0, 0
+    for i in polynomial:
+        if 'x' in i:
+            if len(i) > 1:
+                a += int(i[:-1])
+            else:
+                a +=1
+        else:
+            b += int(i)
+    
+    if a == 0:
+        return '{}'.format(b)
+    elif a == 1:
+        if b == 0:
+            return 'x'
+        elif b != 0:
+            return 'x + {}'.format(b)
+    elif a > 1:
+        if b == 0:
+            return '{}x'.format(a)
+        elif b != 0:
+            return '{}x + {}'.format(a, b)
