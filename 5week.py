cbb3d34c99aa7736112e6184b19908368d3b493e
jongmung <jmp99@naver.com>
2023-04-15 23:32:52 +0900

    codingst5

--- 5week.py
+++ 5week.py
@@ -472,3 +472,8 @@ def solution(n):
                 output += 1
                 break
     return output
+
+# 최댓값 만들기(2)
+def solution(numbers):
+    numbers.sort(reverse = True)
+    return max(numbers[0] * numbers[1],numbers[-1] * numbers[-2])
