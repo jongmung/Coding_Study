1694f2fe7ffbbb536a633a5991eff48ca5322938
jongmung <jmp99@naver.com>
2023-04-19 23:51:39 +0900

    codingst6

--- 6week.py
+++ 6week.py
@@ -42,10 +42,14 @@ def solution(numbers):
     return 45 - sum(numbers)
 
 # 가운데 글자 가져오기
-# 
+# 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요.
+# 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
 def solution(s):
     center = int(len(s)/2)
     if len(s) % 2 != 0:
         return s[center]
     else:
         return s[center-1:center+1]
+# 다른사람 풀이
+def string_middle(str):
+    return str[(len(str)-1)//2 : len(str)//2 + 1]
