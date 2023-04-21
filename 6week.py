e2cc1d4e1b915d4bb9b89c0262d61877951a54a4
jongmung <jmp99@naver.com>
2023-04-21 23:47:19 +0900

    codingst6

--- 6week.py
+++ 6week.py
@@ -83,3 +83,12 @@ def solution(left, right):
         else:
             answer -= i
     return answer
+# 다른사람 풀이
+def solution(left, right):
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
+    return answer
