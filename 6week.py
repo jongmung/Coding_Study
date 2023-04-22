9c4fe97ec6317b0f7632daf80157d34d87d66438
jongmung <jmp99@naver.com>
2023-04-22 23:10:35 +0900

    codingst6

--- 6week.py
+++ 6week.py
@@ -92,3 +92,13 @@ def solution(left, right):
         else:
             answer += i
     return answer
+
+# 부족한 금액 계산하기
+def solution(price, money, count):
+    for i in range(1,count+1):
+        money -= price*i
+    if money < 0:
+        money = abs(money)
+    else:
+        money = 0 
+    return money
