5aca9a8669553c5a3063fab120dd3141047068c3
jongmung <jmp99@naver.com>
2023-04-24 23:53:47 +0900

    codingst6

--- 6week.py
+++ 6week.py
@@ -123,3 +123,11 @@ def solution(n):
     return int(answer, 3)
 # divmod() : 몫과 나머지를 리턴합니다. 리턴 값이 2개이므로 튜플을 사용합니다.
 # int(x, base) : base 진법으로 구성된 str 형식의 수를 10진법으로 변환해 줌
+# 다른사람 풀이
+def solution(n):
+    tmp = ''
+    while n:
+        tmp += str(n % 3)
+        n = n // 3
+    answer = int(tmp, 3)
+    return answer
