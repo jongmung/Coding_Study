db469614d47302ae6902159c5ce350d8e9baafb0
jongmung <jmp99@naver.com>
2023-04-25 23:18:30 +0900

    codingst6

--- 6week.py
+++ 6week.py
@@ -131,3 +131,14 @@ def solution(n):
         n = n // 3
     answer = int(tmp, 3)
     return answer
+
+# 문자열 다루기 기본
+# 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수,
+# solution을 완성하세요. 예를 들어 s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴하면 됩니다.
+def solution(s):
+    if len(s) == 4 and s.isdigit() == 1:
+        return True
+    elif len(s) == 6 and s.isdigit() == 1:
+        return True
+    else:
+        return False
