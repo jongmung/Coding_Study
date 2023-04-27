79c6a95ebed976de414eb49b57dbd2be0b2bd169
jongmung <jmp99@naver.com>
2023-04-27 23:40:56 +0900

    codingst6

--- 6week.py
+++ 6week.py
@@ -189,3 +189,10 @@ def solution(s):
     answer = answer.replace('eight', '8')
     answer = answer.replace('nine', '9')
     return int(answer)
+# 다른사람 풀이
+num_dic = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+def solution(s):
+    answer = s
+    for key, value in num_dic.items():
+        answer = answer.replace(key, value)
+    return int(answer)
