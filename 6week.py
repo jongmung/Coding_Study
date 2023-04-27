a7fab12078f38310e96377c46dfe006861d1e5be
jongmung <jmp99@naver.com>
2023-04-27 23:38:51 +0900

    codingst6

--- 6week.py
+++ 6week.py
@@ -168,3 +168,24 @@ def solution (number) :
             cnt += 1
     return cnt
 
+# 숫자 문자열과 영단어
+# 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
+#   1478 → "one4seveneight"
+#   234567 → "23four5six7"
+#   10203 → "1zerotwozero3"
+# 이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나,
+# 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다.
+# s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+def solution(s):
+    answer = s 
+    answer = answer.replace('zero', '0')
+    answer = answer.replace('one', '1')
+    answer = answer.replace('two', '2')
+    answer = answer.replace('three', '3')
+    answer = answer.replace('four', '4')
+    answer = answer.replace('five', '5')
+    answer = answer.replace('six', '6')
+    answer = answer.replace('seven', '7')
+    answer = answer.replace('eight', '8')
+    answer = answer.replace('nine', '9')
+    return int(answer)
