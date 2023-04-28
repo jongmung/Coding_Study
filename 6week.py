e643ade5359a579993e27df39f6cc253dbf7a9f0
jongmung <jmp99@naver.com>
2023-04-28 23:40:09 +0900

    codingst6

--- 6week.py
+++ 6week.py
@@ -196,3 +196,19 @@ def solution(s):
     for key, value in num_dic.items():
         answer = answer.replace(key, value)
     return int(answer)
+
+# 크기가 작은 부분문자열
+# 숫자로 이루어진 문자열 t와 p가 주어질 때, t에서 p와 길이가 같은 부분문자열 중에서,
+# 이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수를 return하는 함수 solution을 완성하세요.
+# 예를 들어, t="3141592"이고 p="271" 인 경우,
+# t의 길이가 3인 부분 문자열은 314, 141, 415, 159, 592입니다.
+# 이 문자열이 나타내는 수 중 271보다 작거나 같은 수는 141, 159 2개 입니다.
+def solution(t, p):
+  answer = 0
+  p_len = len(p)
+  t_len = len(t)
+  p = int(p)
+  for i in range(0,t_len+1-p_len):
+      if int(t[i:i+p_len]) <= p:
+          answer+=1
+  return answer
