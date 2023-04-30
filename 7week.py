d27e6b826cf41cb52c9ca2fae6a62b854552eafe
jongmung <jmp99@naver.com>
2023-04-30 23:40:20 +0900

    codingst6

--- 7week.py
+++ 7week.py
@@ -10,3 +10,14 @@ def solution(numbers):
     answer = list(answer)
     answer.sort()
     return answer
+# 다른사람 풀이
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    return sorted(list(set(answer)))
+
+from itertools import combinations
+def solution(numbers):
+    return sorted(set(sum(i) for i in list(combinations(numbers, 2))))
