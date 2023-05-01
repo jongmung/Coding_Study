7a0ee466064db71b5d1aa099ef927b0857d53cba
jongmung <jmp99@naver.com>
2023-05-01 23:41:00 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -23,6 +23,10 @@ def solution(numbers):
     return sorted(set(sum(i) for i in list(combinations(numbers, 2))))
 
 # 2016년
+# 2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요?
+# 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수,
+# solution을 완성하세요.
+# 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT
 def solution(a, b):
     answer = 0
     days = ['FRI','SAT','SUN','MON','TUE','WED','THU']
@@ -32,3 +36,8 @@ def solution(a, b):
     answer += b-1
     answer = answer%7    
     return days[answer]
+# 다른사람 풀이
+import datetime
+def getDayName(a,b):
+    t = 'MON TUE WED THU FRI SAT SUN'.split()
+    return t[datetime.datetime(2016, a, b).weekday()]
