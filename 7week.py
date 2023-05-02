f23422b77f7f8a74e21a43eb82892074056ff9ce
jongmung <jmp99@naver.com>
2023-05-02 22:46:41 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -41,3 +41,31 @@ import datetime
 def getDayName(a,b):
     t = 'MON TUE WED THU FRI SAT SUN'.split()
     return t[datetime.datetime(2016, a, b).weekday()]
+
+# 모의고사
+# 수포자는 수학을 포기한 사람의 준말입니다.
+# 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다.
+# 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+#  1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+#  2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+#  3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+# 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때,
+# 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+def solution(answers):   
+    answer = []
+    score = [0,0,0]   
+    student1 = [1,2,3,4,5]
+    student2 = [2,1,2,3,2,4,2,5]
+    student3 = [3,3,1,1,2,2,4,4,5,5]
+    for i in range(len(answers)) :
+        if answers[i] == student1[i%5] :
+            score[0] += 1
+        if answers[i] == student2[i%8] :
+            score[1] += 1
+        if answers[i] == student3[i%10] :
+            score[2] += 1       
+    for idx, num in enumerate(score) :
+        if num == max(score) :
+            answer.append(idx +1)
+    return answer
+
