4860101d46cfe7e340f6db9867e4e2a062036ce1
jongmung <jmp99@naver.com>
2023-05-02 22:49:50 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -68,4 +68,22 @@ def solution(answers):
         if num == max(score) :
             answer.append(idx +1)
     return answer
+# 다른사람 풀이
+def solution(answers):
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+    return result
 
