ed864032f9adddd7126d3db24954d21ffa72c040
jongmung <jmp99@naver.com>
2023-05-03 22:34:17 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -100,3 +100,8 @@ def solution(participant, completion):
         if participant[i] != completion[i]:
             return participant[i]    
     return  participant[-1]
+# 다른사람 풀이
+import collections
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
