c8173a4df6e77b4537a83c4c70f1db2edeecea81
jongmung <jmp99@naver.com>
2023-05-05 23:28:16 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -134,3 +134,16 @@ def solution(X, Y):
 def solution(flo):
     return int(flo)
 
+# flag에 따라 다른 값 반환하기
+# 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때,
+# flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+def solution(a, b, flag):
+    answer = 0
+    if flag == True:
+        answer = a+b
+    else:
+        answer = a-b
+    return answer
+# 다른사람 풀이
+def solution(a, b, flag):
+    return a + b if flag else a - b
