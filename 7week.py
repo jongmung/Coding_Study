3e98a2a7ffe8ed8bc29207e89d58e0c944242233
jongmung <jmp99@naver.com>
2023-05-10 23:58:35 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -177,3 +177,11 @@ def solution(num, n):
 def solution(my_string, n):
     answer = my_string[-n:]
     return answer
+
+# 문자열 곱하기
+# 문자열 my_string과 정수 k가 주어질 때,
+# my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+def solution(my_string, k):
+    answer = my_string * (k)
+    return answer
+
