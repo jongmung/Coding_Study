f1225e811e1653b8cd2e952358a4f4afb2854fc7
jongmung <jmp99@naver.com>
2023-05-12 23:41:57 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -196,3 +196,11 @@ print(str)
 a, b = map(int, input().strip().split(' '))
 print("a =",a)
 print("b =",b)
+
+# 문자열 반복해서 출력하기
+# 문자열 str과 정수 n이 주어집니다.
+# str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+a, b = input().strip().split(' ')
+b = int(b)
+for i in range(0,b):
+    print(a, end='')
