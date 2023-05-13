00089dce53eae95c2caf7047dea80d03ba9700e8
jongmung <jmp99@naver.com>
2023-05-13 23:56:38 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -204,3 +204,9 @@ a, b = input().strip().split(' ')
 b = int(b)
 for i in range(0,b):
     print(a, end='')
+
+# 덧셈식 출력하기
+# 두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
+# a + b = c
+a, b = map(int, input().strip().split(' '))
+print(a,"+",b,"=",a + b)
