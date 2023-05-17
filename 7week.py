1a94d0ac3de2b7f62d662d156605606de9b14fbe
jongmung <jmp99@naver.com>
2023-05-17 23:01:08 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -234,3 +234,8 @@ print(str2)
 # 다른사람 풀이
 print(input().strip().replace(' ', ''))
 
+# 소문자로 바꾸기
+# 알파벳으로 이루어진 문자열 myString이 주어집니다.
+# 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+def solution(myString):
+    return myString.lower()
