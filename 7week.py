57949c199c80e35a6ddfcdd167a512b67914c553
jongmung <jmp99@naver.com>
2023-05-21 23:22:46 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -252,3 +252,15 @@ for i in range(len(str)):
 # 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
 str = input()
 print(str.swapcase())
+
+# 두 수의 연산값 비교하기
+# 연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다.
+# 예를 들면 다음과 같습니다.
+#   12 ⊕ 3 = 123
+#   3 ⊕ 12 = 312
+#양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
+#단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
+def solution(a, b):
+    answer1 = int(str(a) + str(b))
+    answer2 = 2 * a * b
+    return answer1 if answer1 >= answer2 else answer2
