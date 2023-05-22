869fb0d751cb0677e7b1e59563b2a947c54c159b
jongmung <jmp99@naver.com>
2023-05-22 23:10:04 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -264,3 +264,12 @@ def solution(a, b):
     answer1 = int(str(a) + str(b))
     answer2 = 2 * a * b
     return answer1 if answer1 >= answer2 else answer2
+
+# 문자 리스트를 문자열로 변환하기
+# 문자들이 담겨있는 배열 arr가 주어집니다.
+# arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+def solution(arr):
+    answer = ''
+    for i in range(len(arr)):
+        answer += arr[i]
+    return answer
