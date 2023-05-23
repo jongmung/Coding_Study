7022c1d2e40b5b99091fceb93dff1c65319eb0d0
jongmung <jmp99@naver.com>
2023-05-23 23:50:47 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -273,3 +273,14 @@ def solution(arr):
     for i in range(len(arr)):
         answer += arr[i]
     return answer
+
+# 더 크게 합치기
+# 연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+#   12 ⊕ 3 = 123
+#   3 ⊕ 12 = 312
+# 양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+# 단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
+def solution(a, b):
+    answer1 = int(str(a) + str(b))
+    answer2 = int(str(b) + str(a))
+    return answer1 if answer1 >= answer2 else answer2
