2bc28259f960ec8fe5be1ec51eb9b32798b37a3b
jongmung <jmp99@naver.com>
2023-05-24 23:14:10 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -294,6 +294,5 @@ def solution(a, b):
 def solution(str1, str2):
     answer = ''
     for i in range(len(str1)):
-        answer += str1[i]
-        answer += str2[i]
+        answer += str1[i] + str2[i]
     return answer
