3e51285851ca4e3f2f8f49bce14bd41330840e74
jongmung <jmp99@naver.com>
2023-05-25 23:59:26 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -307,4 +307,8 @@ def solution(num_list):
         if num < 0 :
             return i
     return answer
-
+# 다른사람 풀이
+def solution(num_list):
+    for i in range(len(num_list)):
+        if num_list[i]<0:return i
+    return -1
