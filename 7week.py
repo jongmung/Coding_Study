ec833d7bcf07b93ee89e51070429dc2b5e4a5981
jongmung <jmp99@naver.com>
2023-05-26 23:27:07 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -312,3 +312,9 @@ def solution(num_list):
     for i in range(len(num_list)):
         if num_list[i]<0:return i
     return -1
+
+# n 번째 원소까지
+# 정수 리스트 num_list와 정수 n이 주어질 때,
+# num_list의 첫 번째 원소부터 n 번째 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+def solution(num_list, n):
+    return num_list[:n]
