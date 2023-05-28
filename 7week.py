5f3548a4516ce8cf2aca3f255268a9653c52fdc2
jongmung <jmp99@naver.com>
2023-05-28 23:57:46 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -323,3 +323,14 @@ def solution(num_list, n):
 # 정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
 def solution(n):
     return str(n)
+
+# 정수 찾기
+# 정수 리스트 num_list와 찾으려는 정수 n이 주어질 때,
+# num_list안에 n이 있으면 1을 없으면 0을 return하도록 solution 함수를 완성해주세요.
+def solution(num_list, n):
+    answer = 0
+    for i in range(len(num_list)):
+        if num_list[i]==n:
+            answer = 1
+    return answer
+
