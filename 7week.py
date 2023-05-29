bd3bc948995f9b18f24c7b6a0883110d58e7593f
jongmung <jmp99@naver.com>
2023-05-30 01:52:48 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -334,3 +334,11 @@ def solution(num_list, n):
             answer = 1
     return answer
 
+# 대문자로 바꾸기
+# 알파벳으로 이루어진 문자열 myString이 주어집니다.
+# 모든 알파벳을 대문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+def solution(myString):
+    return myString.upper()
+
+
+
