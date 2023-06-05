75d37fbea3095dc5d7d38e0218ad1ee3413bf588
jongmung <jmp99@naver.com>
2023-06-05 23:47:03 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -368,3 +368,14 @@ def solution(num_list, n):
     for i in range(n-1,len(num_list)):
         answer.append(num_list[i])
     return answer
+
+# 문자열 겹쳐쓰기
+# 문자열 my_string, overwrite_string과 정수 s가 주어집니다.
+# 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을
+# 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+def solution(my_string, overwrite_string, s):
+    answer = ''
+    answer = my_string[:s]
+    answer += overwrite_string
+    answer += my_string[len(answer):]
+    return answer
