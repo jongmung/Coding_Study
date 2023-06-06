8493201cdeb62aae270491716dc44a91e765e750
jongmung <jmp99@naver.com>
2023-06-06 23:27:09 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -382,3 +382,19 @@ def solution(my_string, overwrite_string, s):
 # 다른사람 풀이
 def solution(my_string, overwrite_string, s):
     return my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
+
+# 홀짝에 따라 다른 값 반환하기
+# 양의 정수 n이 매개변수로 주어질 때,
+# n이 홀수라면 n 이하의 홀수인 모든 양의 정수의 합을 return 하고
+# n이 짝수라면 n 이하의 짝수인 모든 양의 정수의 제곱의 합을 return 하는 solution 함수를 작성해 주세요.
+def solution(n):
+    answer = 0
+    if n%2 != 0:
+        for i in range(1, n+1):
+            if i%2 != 0:
+                answer += i
+    else:
+        for j in range(1, n+1):
+            if j%2 == 0:
+                answer += j*j           
+    return answer
