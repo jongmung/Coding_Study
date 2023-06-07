b615cf9e1156f3c1a8a51e97cfd84bc82d5dbce6
jongmung <jmp99@naver.com>
2023-06-07 23:00:33 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -401,3 +401,14 @@ def solution(n):
 # 다른사람 풀이
 def solution(n):
     return sum(x ** (2 - x % 2) for x in range(n + 1) if n % 2 == x % 2)
+
+# n보다 커질 때까지 더하기
+# 정수 배열 numbers와 정수 n이 매개변수로 주어집니다.
+# numbers의 원소를 앞에서부터 하나씩 더하다가
+# 그 합이 n보다 커지는 순간 이때까지 더했던 원소들의 합을 return 하는 solution 함수를 작성해 주세요.
+def solution(numbers, n):
+    answer = 0
+    for i in numbers:
+        if answer <= n:
+            answer += i
+    return answer
