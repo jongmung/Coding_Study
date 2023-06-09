88c1d3a196f08e0b528a5dae069ababd936b3c2f
jongmung <jmp99@naver.com>
2023-06-09 23:59:52 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -420,6 +420,8 @@ def solution(n_str):
     return int(n_str)
 
 # 가장 큰 수
+# 0 또는 양의 정수가 주어졌을 때,
+# 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
 def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x * 3, reverse=True)
