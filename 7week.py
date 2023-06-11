df8b51d4badc2c3334a83d4e0f235fe33e9c8692
jongmung <jmp99@naver.com>
2023-06-11 23:29:44 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -443,3 +443,8 @@ def solution(a, b):
     else:
         answer = abs(a-b)
     return answer
+# 다른사람 풀이
+def solution(a, b):
+    if a%2 and b%2: return a*a+b*b
+    elif a%2 or b%2: return 2*(a+b)
+    return abs(a-b)
