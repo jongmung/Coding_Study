db4bb165e500c816ad27d91456dbdadc8e2db23d
jongmung <jmp99@naver.com>
2023-06-11 23:26:54 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -426,3 +426,20 @@ def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x * 3, reverse=True)
     return str(int(''.join(numbers)))
+
+# 주사위 게임1
+# 1부터 6까지 숫자가 적힌 주사위가 두 개 있습니다. 두 주사위를 굴렸을 때 나온 
+# 숫자를 각각 a, b라고 했을 때 얻는 점수는 다음과 같습니다.
+#   a와 b가 모두 홀수라면 a2 + b2 점을 얻습니다.
+#   a와 b 중 하나만 홀수라면 2 × (a + b) 점을 얻습니다.
+#   a와 b 모두 홀수가 아니라면 |a - b| 점을 얻습니다.
+#두 정수 a와 b가 매개변수로 주어질 때, 얻는 점수를 return 하는 solution 함수를 작성해 주세요.
+def solution(a, b):
+    answer = 0
+    if a%2 != 0 and b%2 != 0:
+        answer = a*a + b*b
+    elif a%2 != 0 or b%2 != 0:
+        answer = 2*(a+b)
+    else:
+        answer = abs(a-b)
+    return answer
