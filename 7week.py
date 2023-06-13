736a4c7b09e587704a942c8dfe363596421bc932
jongmung <jmp99@naver.com>
2023-06-13 23:52:06 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -448,3 +448,10 @@ def solution(a, b):
     if a%2 and b%2: return a*a+b*b
     elif a%2 or b%2: return 2*(a+b)
     return abs(a-b)
+
+# 뒤에서 5등까지
+# 정수로 이루어진 리스트 num_list가 주어집니다.
+# num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+def solution(num_list):
+    num_list.sort()
+    return num_list[:5]
