a442c620cb4316f1f21b5fb5442933f48b117828
jongmung <jmp99@naver.com>
2023-06-17 23:37:28 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -483,3 +483,12 @@ def solution(start, end):
     for i in range(start, end+1):
         answer.append(i)
     return answer
+
+# 카운트 다운
+# 정수 start와 end가 주어질 때,
+# start에서 end까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+def solution(start, end):
+    answer = []
+    for i in range(start, end-1, -1):
+        answer.append(i)
+    return answer
