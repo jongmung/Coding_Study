85734041fccfb15dc3911e10e95030a1bc6740c1
jongmung <jmp99@naver.com>
2023-06-18 23:15:19 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -492,3 +492,10 @@ def solution(start, end):
     for i in range(start, end-1, -1):
         answer.append(i)
     return answer
+
+# 배열 만들기1
+# 정수 n과 k가 주어졌을 때,
+# 1 이상 n이하의 정수 중에서 k의 배수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+def solution(n, k):
+    answer = [i for i in range(1, n+1) if i % k == 0]
+    return answer
