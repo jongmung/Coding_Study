bd75ef8e1f4d08046e0b77fb74b5eda2249b4a33
jongmung <jmp99@naver.com>
2023-06-20 23:56:16 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -506,3 +506,11 @@ def solution(n, k):
 # rny_string의 모든 'm'을 "rn"으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
 def solution(rny_string):
     return rny_string.replace('m','rn')
+
+# 공백으로 구분하기1
+# 단어가 공백 한 개로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때,
+# my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+def solution(my_string):
+    answer = []
+    answer = my_string.split()
+    return answer
