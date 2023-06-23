b777c6837f7daee345fd42d914ab3605a3a91e1d
jongmung <jmp99@naver.com>
2023-06-23 23:39:25 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -534,3 +534,10 @@ def solution(str_list, ex):
         if not ex in some_string:
             answer += some_string
     return answer
+
+# A 강조하기
+# 문자열 myString이 주어집니다.
+# myString에서 알파벳 "a"가 등장하면 전부 "A"로 변환하고,
+# "A"가 아닌 모든 대문자 알파벳은 소문자 알파벳으로 변환하여 return 하는 solution 함수를 완성하세요.
+def solution(myString):
+    return (myString.lower()).replace('a', 'A')
