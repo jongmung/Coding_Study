9b327cc1740cb241a1a7ac233a659b9bcaa53658
jongmung <jmp99@naver.com>
2023-07-02 23:58:49 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -624,3 +624,10 @@ def solution(date1, date2):
 # 이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성해주세요.
 def solution(number):
     return int(number)%9
+
+# 문자열 잘라서 정렬하기
+# 문자열 myString이 주어집니다.
+# "x"를 기준으로 해당 문자열을 잘라내 배열을 만든 후 사전순으로 정렬한 배열을 return 하는 solution 함수를 완성해 주세요.
+def solution(myString):
+    answer = list(filter(None, myString.split("x")))
+    return sorted(answer)
