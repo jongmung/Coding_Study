077d0edbc4a7951e296335d4ee949abac639bdba
jongmung <jmp99@naver.com>
2023-07-03 23:54:49 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -631,3 +631,14 @@ def solution(number):
 def solution(myString):
     answer = list(filter(None, myString.split("x")))
     return sorted(answer)
+
+# 가까운 1찾기
+# 정수 배열 arr가 주어집니다. 이때 arr의 원소는 1 또는 0입니다.
+# 정수 idx가 주어졌을 때, idx보다 크면서 배열의 값이 1인 가장 작은 인덱스를 찾아서 반환하는 solution 함수를 완성해 주세요.
+# 단, 만약 그러한 인덱스가 없다면 -1을 반환합니다.
+def solution(arr, idx):
+    answer = -1
+    for i, num in enumerate(arr):
+        if idx <= i and num == 1:
+            return i
+    return answer
