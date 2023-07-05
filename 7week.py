67aa58e5a85d4e0d1caf6f7b11c506f08c7400ba
jongmung <jmp99@naver.com>
2023-07-05 22:45:59 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -650,3 +650,15 @@ def solution(arr, idx):
 def solution(my_string, m, c):
     return my_string[c-1::m]
 
+# 홀수 vs 짝수
+# 정수 리스트 num_list가 주어집니다. 가장 첫 번째 원소를 1번 원소라고 할 때,
+# 홀수 번째 원소들의 합과 짝수 번째 원소들의 합 중 큰 값을 return 하도록 solution 함수를 완성해주세요.
+# 두 값이 같을 경우 그 값을 return합니다.
+def solution(num_list):
+    odd_sum = sum(num_list[1::2])
+    even_sum = sum(num_list[0::2])
+    if odd_sum<even_sum:
+        return even_sum
+    else:
+        return odd_sum
+
