c4f7b5f18e9826f0fea94c7490d7437aad27b524
jongmung <jmp99@naver.com>
2023-07-06 23:15:01 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -668,7 +668,6 @@ def solution(num_list):
 # 마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여
 # return하도록 solution 함수를 완성해주세요.
 def solution(num_list):
-    answer = []
     if num_list[-1] > num_list[-2]:
         num_list.append(num_list[-1]-num_list[-2])
     else:
