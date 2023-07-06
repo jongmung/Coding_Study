3f064447f2f28f4beb95d271408b8adf3f81d91e
jongmung <jmp99@naver.com>
2023-07-06 23:14:46 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -662,3 +662,15 @@ def solution(num_list):
     else:
         return odd_sum
 
+# 마지막 두 원소
+# 정수 리스트 num_list가 주어질 때,
+# 마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을
+# 마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여
+# return하도록 solution 함수를 완성해주세요.
+def solution(num_list):
+    answer = []
+    if num_list[-1] > num_list[-2]:
+        num_list.append(num_list[-1]-num_list[-2])
+    else:
+        num_list.append(2*num_list[-1])
+    return num_list
