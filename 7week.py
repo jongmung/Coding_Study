9486b0c5210ca1e669b13aa81c34003085afbbf4
jongmung <jmp99@naver.com>
2023-07-07 22:19:41 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -673,3 +673,9 @@ def solution(num_list):
     else:
         num_list.append(2*num_list[-1])
     return num_list
+
+# 0 떼기
+# 정수로 이루어진 문자열 n_str이 주어질 때,
+# n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해주세요.
+def solution(n_str):
+    return n_str.lstrip('0')
