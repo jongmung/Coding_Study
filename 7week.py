b6a54b304e71d6db84c1f670fdefce0331579bab
jongmung <jmp99@naver.com>
2023-07-08 23:59:30 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -679,3 +679,21 @@ def solution(num_list):
 # n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return하도록 solution 함수를 완성해주세요.
 def solution(n_str):
     return n_str.lstrip('0')
+
+# 주사위 게임 2
+# 1부터 6까지 숫자가 적힌 주사위가 세 개 있습니다.
+# 세 주사위를 굴렸을 때 나온 숫자를 각각 a, b, c라고 했을 때 얻는 점수는 다음과 같습니다.
+#   세 숫자가 모두 다르다면 a + b + c 점을 얻습니다.
+#   세 숫자 중 어느 두 숫자는 같고 나머지 다른 숫자는 다르다면 (a + b + c) × (a2 + b2 + c2 )점을 얻습니다.
+#   세 숫자가 모두 같다면 (a + b + c) × (a2 + b2 + c2 ) × (a3 + b3 + c3 )점을 얻습니다.
+# 세 정수 a, b, c가 매개변수로 주어질 때, 얻는 점수를 return 하는 solution 함수를 작성해 주세요.
+def solution(a, b, c):
+    answer = 0
+    one, two, three = (a + b + c), (a**2 + b**2 + c**2), (a**3 + b**3 + c**3)
+    if a == b and b == c :
+        answer = one * two * three
+    elif a == b or b == c or a == c:
+        answer = one * two
+    else:
+        answer = one
+    return answer
