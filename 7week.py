8fad43cc01ad6caec24b98d61542d97c1b687924
jongmung <jmp99@naver.com>
2023-07-11 22:19:45 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -719,3 +719,15 @@ def solution(ineq, eq, n, m):
     elif (ineq == '<' and eq == '!'):
         answer = 1 if n < m else 0
     return answer
+
+# 배열 만들기 5
+# 배열 intStrs의 각 원소마다 s번 인덱스에서 시작하는 길이 l짜리 부분 문자열을 잘라내 정수로 변환합니다.
+# 이때 변환한 정수값이 k보다 큰 값들을 담은 배열을 return 하는 solution 함수를 완성해 주세요.
+def solution(intStrs, k, s, l):
+    answer = []
+    
+    for i in intStrs:
+        if int(i[s:s+l]) > k:
+            answer.append(int(i[s:s+l]))
+    
+    return answer
