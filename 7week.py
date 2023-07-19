18547399afc6e751229bb1ed898e2a4e7545eddf
jongmung <jmp99@naver.com>
2023-07-19 22:29:05 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -845,3 +845,9 @@ def solution(picture, k):
         for _ in range(k):
             answer.append(resized)
     return answer
+
+# 두 수의 합
+# 0 이상의 두 정수가 문자열 a, b로 주어질 때,
+# a + b의 값을 문자열로 return 하는 solution 함수를 작성해 주세요.
+def solution(a, b):
+    return str(int(a)+int(b))
