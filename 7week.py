64c805b260cb00d2e1f3a026147f871f2172b3e6
jongmung <jmp99@naver.com>
2023-07-30 11:50:08 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -967,3 +967,11 @@ def solution(n, control):
         elif k == "a":
             answer -= 10    
     return answer
+
+# 문자열 바꿔서 찾기
+# 문자 "A"와 "B"로 이루어진 문자열 myString과 pat가 주어집니다.
+# myString의 "A"를 "B"로, "B"를 "A"로 바꾼 문자열의 연속하는 부분 문자열 중 pat이 있으면 1을
+# 아니면 0을 return 하는 solution 함수를 완성하세요.
+def solution(myString, pat):
+    tmp = "".join(["B" if char == "A" else "A" for char in myString])
+    return 1 if pat in tmp else 0
