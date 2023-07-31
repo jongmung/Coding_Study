9dffade69dbdc150296e0a2011cdbfa96d2f40ec
jongmung <jmp99@naver.com>
2023-07-31 20:05:07 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -975,3 +975,16 @@ def solution(n, control):
 def solution(myString, pat):
     tmp = "".join(["B" if char == "A" else "A" for char in myString])
     return 1 if pat in tmp else 0
+
+# 문자 개수 세기
+# 알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때, my_string에서 'A'의 개수,
+# my_string에서 'B'의 개수,..., my_string에서 'Z'의 개수, my_string에서 'a'의 개수,
+# my_string에서 'b'의 개수,..., my_string에서 'z'의 개수를
+# 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
+def solution(my_string):
+    answer = [0 for i in range(52)]
+    for string in my_string:
+        if string.isupper(): k = 65
+        else: k = 71
+        answer[ord(string)-k] += 1
+    return answer
