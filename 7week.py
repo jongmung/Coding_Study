2060a38c1175578dcb9defb33bc94351ebe4124a
jongmung <jmp99@naver.com>
2023-08-02 23:34:50 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -988,3 +988,15 @@ def solution(my_string):
         else: k = 71
         answer[ord(string)-k] += 1
     return answer
+
+# 문자열 뒤집기
+# 문자열 my_string과 정수 s, e가 매개변수로 주어질 때, my_string에서
+# 인덱스 s부터 인덱스 e까지를 뒤집은 문자열을 return 하는 solution 함수를 작성해 주세요.
+def solution(my_string, s, e):
+    answer = ''
+    a = ''
+    answer = my_string[:s]
+    a = my_string[s:e+1:]
+    a = a[::-1]
+    answer += a+ my_string[e+1:]
+    return answer
