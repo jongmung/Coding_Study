9510573b2d20df914de37dac1bd3b1a6122002a8
jongmung <jmp99@naver.com>
2023-08-05 23:33:11 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -1030,3 +1030,16 @@ def solution(num_list):
         for i in num_list:
             answer *= i
     return answer
+
+# 접두사인지 확인하기
+# 어떤 문자열에 대해서 접두사는 특정 인덱스까지의 문자열을 의미합니다.
+# 예를 들어, "banana"의 모든 접두사는 "b", "ba", "ban", "bana", "banan", "banana"입니다.
+# 문자열 my_string과 is_prefix가 주어질 때, is_prefix가 my_string의 접두사라면 1을,
+# 아니면 0을 return 하는 solution 함수를 작성해 주세요.
+def solution(my_string, is_prefix):
+    answer = 0
+    tmp = [my_string[:i] for i in range(0, len(my_string))]
+    for i in tmp:
+        if is_prefix == i:
+            answer = 1
+    return answer
