5b5003a78f7f5d35001412131554596edda666a5
jongmung <jmp99@naver.com>
2023-08-08 23:14:57 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -1054,3 +1054,13 @@ def solution(my_string, is_suffix):
 # is_suffix가 my_string의 접미사라면 1을, 아니면 0을 return 하는 solution 함수를 작성해 주세요.
 def solution(my_string, is_suffix):
     return int(my_string.endswith(is_suffix))
+
+# qr code
+# 두 정수 q, r과 문자열 code가 주어질 때,
+# code의 각 인덱스를 q로 나누었을 때 나머지가 r인 위치의 문자를 앞에서부터 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+def solution(q, r, code):
+    answer = ''
+    for idx, val in enumerate(code):
+        if idx % q == r:
+            answer += val
+    return answer
