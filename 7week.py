4427dcbf348623a462375debb2d4414cdec66955
jongmung <jmp99@naver.com>
2023-08-10 21:50:39 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -1080,3 +1080,17 @@ def solution(order):
         else:
             answer += 4500
     return answer
+
+# 문자열이 몇 번 등장하는지 세기
+# 문자열 myString과 pat이 주어집니다.
+# myString에서 pat이 등장하는 횟수를 return 하는 solution 함수를 완성해 주세요.
+def solution(myString, pat):    
+    a = 0
+    cnt = 0
+    while True:
+        idx = myString.find(pat, a)
+        if idx == -1:
+            break
+        cnt += 1
+        a = idx + 1
+    return cnt
