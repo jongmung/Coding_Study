a152083c35bf2db9f7785a7cf67f775acb01d5a6
jongmung <jmp99@naver.com>
2023-08-12 23:04:18 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -1116,6 +1116,8 @@ def solution(arr):
     return answer
 
 # 부분 문자열인지 확인하기
+# 예를 들어, 문자열 "ana", "ban", "anana", "banana", "n"는 모두 문자열 "banana"의 부분 문자열이지만,
+# "aaa", "bnana", "wxyz"는 모두 "banana"의 부분 문자열이 아닙니다.
 # 문자열 my_string과 target이 매개변수로 주어질 때,
 # target이 문자열 my_string의 부분 문자열이라면 1을,
 # 아니라면 0을 return 하는 solution 함수를 작성해 주세요.
