a3ff5925abc562fdf49610bab53022906557eae3
jongmung <jmp99@naver.com>
2023-08-12 23:03:55 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -1114,3 +1114,13 @@ def solution(arr):
     else:
         answer = arr
     return answer
+
+# 부분 문자열인지 확인하기
+# 문자열 my_string과 target이 매개변수로 주어질 때,
+# target이 문자열 my_string의 부분 문자열이라면 1을,
+# 아니라면 0을 return 하는 solution 함수를 작성해 주세요.
+def solution(my_string, target):
+    answer = 0
+    if target in my_string:
+        return 1
+    return answer
