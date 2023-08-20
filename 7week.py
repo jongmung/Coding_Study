73eedc22ed1d6f15c5bca4ff40ff42e81e21300a
jongmung <jmp99@naver.com>
2023-08-20 23:10:50 +0900

    codingst7

--- 7week.py
+++ 7week.py
@@ -1207,3 +1207,13 @@ def solution(numLog):
         if idx != len(numLog)-1: 
             answer += dic[numLog[idx+1] - numLog[idx]]
     return answer
+
+# n개 간격의 원소들
+# 정수 리스트 num_list와 정수 n이 주어질 때,
+# num_list의 첫 번째 원소부터 마지막 원소까지
+# n개 간격으로 저장되어있는 원소들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+def solution(num_list, n): 
+    return [num_list[i] for i in range(0, len(num_list), n)]
+# 다른사람 풀이
+def solution(num_list, n):
+    return num_list[::n]
