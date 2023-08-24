f56016d01439ae939be4f5e0521ed7093bda6067
jongmung <jmp99@naver.com>
2023-08-24 21:37:05 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -35,3 +35,14 @@ def solution(arr, k):
         else:
             answer.append(int(i)*k)
     return answer
+
+# 수열과 구간 쿼리1
+# 정수 배열 arr와 2차원 정수 배열 queries이 주어집니다.
+# queries의 원소는 각각 하나의 query를 나타내며, [s, e] 꼴입니다.
+# 각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 arr[i]에 1을 더합니다.
+# 위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
+def solution(arr, queries):
+    for i in queries:
+        increse = [x + 1 for x in arr[i[0]:i[1]+1]]
+        arr[i[0]:i[1]+1] = increse
+    return arr
