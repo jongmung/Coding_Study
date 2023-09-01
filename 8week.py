b1fe485c6e8aeddeaebe386ea7c407fb72746363
jongmung <jmp99@naver.com>
2023-09-01 23:35:43 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -137,3 +137,9 @@ def solution(arr):
         length = length / 2
         count += 1
     return arr + [0] * (2 ** count - len(arr))
+
+# 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기
+# 문자열 myString과 pat가 주어집니다.
+# myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 찾아서 return 하는 solution 함수를 완성해 주세요.
+def solution(myString, pat):
+    return myString.rsplit(pat,1)[0]+pat
