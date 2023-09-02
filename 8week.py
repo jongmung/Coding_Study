491d5e4044336cc71c7ca5bbb0ff8ee73815eedf
jongmung <jmp99@naver.com>
2023-09-02 23:11:20 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -143,3 +143,27 @@ def solution(arr):
 # myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 찾아서 return 하는 solution 함수를 완성해 주세요.
 def solution(myString, pat):
     return myString.rsplit(pat,1)[0]+pat
+
+# 조건에 맞게 수열 변환하기 2
+# 정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고,
+# 50보다 작은 홀수라면 2를 곱하고 다시 1을 더합니다.
+# 이러한 작업을 x번 반복한 결과인 배열을 arr(x)라고 표현했을 때,
+# arr(x) = arr(x + 1)인 x가 항상 존재합니다.
+# 이러한 x 중 가장 작은 값을 return 하는 solution 함수를 완성해 주세요.
+# 단, 두 배열에 대한 "="는 두 배열의 크기가 서로 같으며,
+# 같은 인덱스의 원소가 각각 서로 같음을 의미합니다.
+def solution(arr):
+    idx = 0
+    prev = arr
+    while True:
+        change = []
+        for i in prev:
+            if i >= 50 and i % 2 == 0: change.append(int(i / 2))
+            elif i < 50 and i % 2 == 1: change.append(i * 2 + 1)
+            else: change.append(i)
+        same = all(a == b for a, b in zip(prev, change))
+        if same:
+            break
+        idx += 1
+        prev = change
+    return idx
