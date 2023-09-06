3c196cf8a903a437447cd4f5d6f5722b55b38f33
jongmung <jmp99@naver.com>
2023-09-06 22:14:22 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -227,6 +227,12 @@ def solution(strArr):
         if idx % 2 == 0:
             answer.append(val.lower())
         else:
-            answer.append(val.upper())
-    
+            answer.append(val.upper())    
     return answer
+
+# x 사이의 개수
+# 문자열 myString이 주어집니다. myString을 문자 "x"를 기준으로 나눴을 때
+# 나눠진 문자열 각각의 길이를 순서대로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+def solution(myString):
+    tmp = myString.split("x")
+    return [len(i) for i in tmp]
