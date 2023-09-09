2a09b0da7f738bdbaa8c8f352edbf454e626f8f8
jongmung <jmp99@naver.com>
2023-09-09 23:54:49 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -272,3 +272,15 @@ def solution(arr, k):
         answer = arr[:k]
         answer += [-1]*(k-len(arr))
     return answer
+
+# l로 만들기
+# 알파벳 소문자로 이루어진 문자열 myString이 주어집니다.
+# 알파벳 순서에서 "l"보다 앞서는 모든 문자를 "l"로 바꾼 문자열을 return 하는 solution 함수를 완성해 주세요.
+def solution(myString):
+    chars = { "a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25 }
+    char = ""
+    for i in myString:
+        if chars[i] < 11:
+            i = "l"
+        char += i
+    return char
