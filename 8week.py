1ccb1d04e9dfad00fefc512e89ea7950e41a6b46
jongmung <jmp99@naver.com>
2023-09-10 23:54:00 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -284,3 +284,11 @@ def solution(myString):
             i = "l"
         char += i
     return char
+
+# 할 일 목록
+# 오늘 해야 할 일이 담긴 문자열 배열 todo_list와
+# 각각의 일을 지금 마쳤는지를 나타내는 boolean 배열 finished가 매개변수로 주어질 때,
+# todo_list에서 아직 마치지 못한 일들을 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+def solution(todo_list, finished):
+    answer = dict(zip(todo_list, finished))
+    return [key for key, val in answer.items() if val is False]
