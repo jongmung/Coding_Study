c3468f946f41685b02e6cfe0bfc3e9926a27ba48
jongmung <jmp99@naver.com>
2023-09-14 22:43:52 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -358,3 +358,13 @@ def solution(arr, flag):
              for k in range(arr[i]):
                 answer.pop(-1)
     return answer
+# 다른사람 풀이
+def solution(arr, flag):
+    X = []
+    for i, f in enumerate(flag):
+        if f:
+            X += [arr[i]] * (arr[i]*2)
+        else:
+            for _ in range(arr[i]):
+                X.pop()
+    return X
