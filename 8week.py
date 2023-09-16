9e466d733f1da5a185caaee4c1cc7f284f7d4e27
jongmung <jmp99@naver.com>
2023-09-16 23:58:13 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -378,3 +378,14 @@ def solution(my_string, indices):
     for i in indices:
         answer[i]=""
     return "".join(answer)
+    
+# 전국 대회 선발 고사
+def solution(rank, attendance):
+    n = len(rank)
+    answer =0
+    r_a = []
+    for i in range(n):
+        if attendance[i]:
+            r_a.append([rank[i], i])
+    r_a.sort(key = lambda v : v[0])
+    return 10000 * r_a[0][1] + 100 * r_a[1][1] + r_a[2][1]
