eaaace0a2034900e59ccda6bf6a757e2f42ab1c3
jongmung <jmp99@naver.com>
2023-09-17 23:22:14 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -396,3 +396,18 @@ def solution(rank, attendance):
             r_a.append([rank[i], i])
     r_a.sort(key = lambda v : v[0])
     return 10000 * r_a[0][1] + 100 * r_a[1][1] + r_a[2][1]
+
+# 최소직사각형
+# 모든 명함의 가로 길이와 세로 길이를 나타내는 2차원 배열 sizes가 매개변수로 주어집니다.
+# 모든 명함을 수납할 수 있는 가장 작은 지갑을 만들 때, 지갑의 크기를 return 하도록 solution 함수를 완성해주세요.
+def solution(sizes):
+    w = []
+    h = []
+    for i in range(len(sizes)):
+        if sizes[i][0] >= sizes[i][1]:
+            w.append(sizes[i][0])
+            h.append(sizes[i][1])
+        else:
+            h.append(sizes[i][0])
+            w.append(sizes[i][1])
+    return max(w) * max(h)
