8a1bf5317cb86b13fdc219a322b1e63767c2759c
jongmung <jmp99@naver.com>
2023-09-22 19:33:23 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -489,3 +489,21 @@ def solution(food):
     for i in range(1, len(food)):
         temp += str(i) * (food[i]//2)
     return temp + '0' + temp[::-1]
+
+# 카드 뭉치
+#  원하는 카드 뭉치에서 카드를 순서대로 한 장씩 사용합니다.
+#  한 번 사용한 카드는 다시 사용할 수 없습니다.
+#  카드를 사용하지 않고 다음 카드로 넘어갈 수 없습니다.
+#  기존에 주어진 카드 뭉치의 단어 순서는 바꿀 수 없습니다.
+# 문자열로 이루어진 배열 cards1, cards2와 원하는 단어 배열 goal이 매개변수로 주어질 때,
+# cards1과 cards2에 적힌 단어들로 goal를 만들 있다면 "Yes"를,
+# 만들 수 없다면 "No"를 return하는 solution 함수를 완성해주세요.
+def solution(cards1, cards2, goal):
+    for g in goal:
+        if len(cards1) > 0 and g == cards1[0]:
+            cards1.pop(0)       
+        elif len(cards2) > 0 and g == cards2[0]:
+            cards2.pop(0)
+        else:
+            return "No"
+    return "Yes"
