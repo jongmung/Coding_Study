70f15df99e6b617f8308eb91203518219def50fe
jongmung <jmp99@naver.com>
2023-09-30 23:55:40 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -639,3 +639,25 @@ def solution(number, limit, power):
                 break
         kg.append(cnt)
     return sum(kg)
+
+# 체육복
+# 학생들의 번호는 체격 순으로 매겨져 있어, 바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다.
+# 전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost,
+# 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때,
+# 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+def solution(n, lost, reserve):
+    # 정렬
+    lost.sort()
+    reserve.sort()
+    # lost, reserve에 공통으로 있는 요소 제거
+    for i in reserve[:]:
+        if i in lost:
+            reserve.remove(i)
+            lost.remove(i)
+    # 체육복 빌려주기(나의 앞 번호부터 확인)
+    for i in reserve:
+        if i-1 in lost:
+            lost.remove(i-1)
+        elif i+1 in lost:
+            lost.remove(i+1)
+    return n-len(lost)
