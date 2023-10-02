5220d3dad62a23eb134e7aba71f827fcf00bc5f7
jongmung <jmp99@naver.com>
2023-10-02 09:41:46 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -683,3 +683,15 @@ def solution(s):
         else:
             cnt2 += 1            
     return answer
+
+# 예산
+# 물품을 구매해 줄 때는 각 부서가 신청한 금액만큼을 모두 지원해 줘야 합니다.
+# 예를 들어 1,000원을 신청한 부서에는 정확히 1,000원을 지원해야 하며,
+# 1,000원보다 적은 금액을 지원해 줄 수는 없습니다.
+# 부서별로 신청한 금액이 들어있는 배열 d와 예산 budget이 매개변수로 주어질 때,
+# 최대 몇 개의 부서에 물품을 지원할 수 있는지 return 하도록 solution 함수를 완성해주세요.
+def solution(d, budget):
+    d.sort()
+    while budget < sum(d):
+        d.pop()
+    return len(d)
