2a730d0b4a924146cf8fd2dfc33bca4477d10349
jongmung <jmp99@naver.com>
2023-10-11 23:56:16 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -875,3 +875,17 @@ def solution(players, callings):
         result[players[idx-1]] += 1 # 앞에 위치했던 선수의 등수 +1
         players[idx-1], players[idx] = players[idx], players[idx-1] # 위치 변경
     return players
+
+# 성격 유형 검사하기
+def solution(survey, choices):
+    answer = ''
+    dic= {"R" : 0,"T" : 0,"C" : 0,"F" : 0,"J" : 0,"M" : 0,"A" : 0,"N" : 0 }
+    for s,c in zip(survey,choices):
+        if c>4:     dic[s[1]] += c-4
+        elif c<4:   dic[s[0]] += 4-c
+    
+    li = list(dic.items())
+    for i in range(0,8,2):
+        if li[i][1] < li[i+1][1]: answer += li[i+1][0]
+        else:   answer += li[i][0]
+    return answer
