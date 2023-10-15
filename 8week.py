297897eb751bac8e09ccc97df440d6e5880db984
jongmung <jmp99@naver.com>
2023-10-15 23:19:04 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -922,3 +922,11 @@ def solution(board, moves):
                         answer += 2     
                 break
     return answer
+
+# 최댓값과 최솟값
+# 문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다.
+# str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수,
+# solution을 완성하세요.
+def solution(s):
+    arr = list(map(int, s.split(' ')))
+    return str(min(arr)) + " " + str(max(arr))
