5a7f2ad7517b0cc6b59fb01fb1efdb84ce6562ad
jongmung <jmp99@naver.com>
2023-11-06 23:40:50 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -1267,3 +1267,37 @@ def solution(n, left, right):
         if a<b: a,b =b,a # 큰거 구하기 
         answer.append(a+1)
     return answer
+
+# 영어 끝말잇기
+# 1부터 n까지 번호가 붙어있는 n명의 사람이 영어 끝말잇기를 하고 있습니다. 영어 끝말잇기는 다음과 같은 규칙으로 진행됩니다.
+#   1번부터 번호 순서대로 한 사람씩 차례대로 단어를 말합니다.
+#   마지막 사람이 단어를 말한 다음에는 다시 1번부터 시작합니다.
+#   앞사람이 말한 단어의 마지막 문자로 시작하는 단어를 말해야 합니다.
+#   이전에 등장했던 단어는 사용할 수 없습니다.
+#   한 글자인 단어는 인정되지 않습니다.
+# 다음은 3명이 끝말잇기를 하는 상황을 나타냅니다.
+# tank → kick → know → wheel → land → dream → mother → robot → tank
+# 위 끝말잇기는 다음과 같이 진행됩니다.
+#   1번 사람이 자신의 첫 번째 차례에 tank를 말합니다.
+#   2번 사람이 자신의 첫 번째 차례에 kick을 말합니다.
+#   3번 사람이 자신의 첫 번째 차례에 know를 말합니다.
+#   1번 사람이 자신의 두 번째 차례에 wheel을 말합니다.
+#   (계속 진행)
+# 끝말잇기를 계속 진행해 나가다 보면, 3번 사람이 자신의 세 번째 차례에 말한 tank 라는 단어는 이전에 등장했던 단어이므로 탈락하게 됩니다.
+# 사람의 수 n과 사람들이 순서대로 말한 단어 words 가 매개변수로 주어질 때,
+# 가장 먼저 탈락하는 사람의 번호와 그 사람이 자신의 몇 번째 차례에 탈락하는지를 구해서 return 하도록 solution 함수를 완성해주세요.
+def solution(n, words):
+    answer = [0,0]
+    cnt = 0  # 탈락번호,차례 계산할 변수
+    checks = []  # 나온 단어 확인할 리스트
+    checks.append(words[0])
+    for i in range(1, len(words)):  # 단어 순회하면서
+        cnt += 1
+        # 아직 안나온 단어이면서 & 앞 단어의 마지막 알파벳과 일치하면 checks 리스트에 넣음 (pass)
+        if words[i] not in checks and list(words[i-1])[-1] == list(words[i])[0]:
+            checks.append(words[i])
+        else:  # (fail)
+            answer[0] = cnt%n +1  # 탈락번호
+            answer[1] = cnt//n +1  # 탈락차례
+            break
+    return answer
