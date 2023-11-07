069996ac50a2c8920dbc01cde113f526c46a3eb2
jongmung <jmp99@naver.com>
2023-11-07 23:03:11 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -1301,3 +1301,58 @@ def solution(n, words):
             answer[1] = cnt//n +1  # 탈락차례
             break
     return answer
+
+# 공원 산책
+# 지나다니는 길을 'O', 장애물을 'X'로 나타낸 직사각형 격자 모양의 공원에서 로봇 강아지가 산책을 하려합니다.
+# 산책은 로봇 강아지에 미리 입력된 명령에 따라 진행하며, 명령은 다음과 같은 형식으로 주어집니다.
+#   ["방향 거리", "방향 거리" … ]
+# 예를 들어 "E 5"는 로봇 강아지가 현재 위치에서 동쪽으로 5칸 이동했다는 의미입니다.
+# 로봇 강아지는 명령을 수행하기 전에 다음 두 가지를 먼저 확인합니다.
+#   주어진 방향으로 이동할 때 공원을 벗어나는지 확인합니다.
+#   주어진 방향으로 이동 중 장애물을 만나는지 확인합니다.
+# 위 두 가지중 어느 하나라도 해당된다면, 로봇 강아지는 해당 명령을 무시하고 다음 명령을 수행합니다.
+# 공원의 가로 길이가 W, 세로 길이가 H라고 할 때, 공원의 좌측 상단의 좌표는 (0, 0), 우측 하단의 좌표는 (H - 1, W - 1) 입니다.
+# 공원을 나타내는 문자열 배열 park, 로봇 강아지가 수행할 명령이 담긴 문자열 배열 routes가 매개변수로 주어질 때,
+# 로봇 강아지가 모든 명령을 수행 후 놓인 위치를 [세로 방향 좌표, 가로 방향 좌표] 순으로 배열에 담아 return 하도록 solution 함수를 완성해주세요.
+def solution(park, routes):
+    # 위치 index
+    x = 0
+    y = 0 
+    
+    # 시작 위치 찾기
+    for i in range(len(park)):
+        for j in range(len(park[i])):
+            if park[i][j] == 'S':
+                x = j
+                y = i
+                break
+    
+    # 이동
+    for route in routes:
+        # 위치 초기화
+        xx = x
+        yy = y
+        # 이동 - 장애물이 있거나 공원을 벗어나면 명령 무시
+        for step in range(int(route[2])):
+            # 동쪽 : 현재 위치가 map 가장 오른쪽이면 안됨, 이동할 곳이 장애물이면 안됨
+            if route[0] == 'E' and xx != len(park[0])-1 and park[yy][xx+1] != 'X':
+                xx += 1
+                if step == int(route[2])-1:
+                    x = xx # step만큼 움직였으면 위치 초기화
+            # 서쪽 : 현재 위치가 map 가장 왼쪽이면 안됨, 이동할 곳이 장애물이면 안됨
+            elif route[0] == 'W' and xx != 0 and park[yy][xx-1] != 'X':
+                xx -= 1
+                if step == int(route[2])-1:
+                    x = xx
+            # 남쪽 : 현재 위치가 map 가장 아래쪽이면 안됨, 이동할 곳이 장애물이면 안됨
+            elif route[0] == 'S' and yy != len(park)-1 and park[yy+1][xx] != 'X':
+                yy += 1
+                if step == int(route[2])-1:
+                    y = yy
+            # 북쪽 : 현재 위치가 map 가장 위쪽이면 안됨, 이동할 곳이 장애물이면 안됨
+            elif route[0] == 'N' and yy != 0 and park[yy-1][xx] != 'X':
+                yy -= 1
+                if step == int(route[2])-1:
+                    y = yy
+                    
+    return [y, x]
