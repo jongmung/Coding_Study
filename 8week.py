9f5621138d8c172dbe7608c89330edb0f8656d4c
jongmung <jmp99@naver.com>
2023-11-08 23:17:17 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -1356,3 +1356,25 @@ def solution(park, routes):
                     y = yy
                     
     return [y, x]
+
+# 할인 행사
+# XYZ 마트는 일정한 금액을 지불하면 10일 동안 회원 자격을 부여합니다.
+# XYZ 마트에서는 회원을 대상으로 매일 한 가지 제품을 할인하는 행사를 합니다.
+# 할인하는 제품은 하루에 하나씩만 구매할 수 있습니다.
+# 알뜰한 정현이는 자신이 원하는 제품과 수량이 할인하는 날짜와 10일 연속으로 일치할 경우에 맞춰서 회원가입을 하려 합니다.
+# 정현이가 원하는 제품을 나타내는 문자열 배열 want와
+# 정현이가 원하는 제품의 수량을 나타내는 정수 배열 number,
+# XYZ 마트에서 할인하는 제품을 나타내는 문자열 배열 discount가 주어졌을 때,
+# 회원등록시 정현이가 원하는 제품을 모두 할인 받을 수 있는 회원등록 날짜의 총 일수를 return 하는 solution 함수를 완성하시오.
+# 가능한 날이 없으면 0을 return 합니다.
+from collections import Counter
+def solution(want, number, discount):
+    best_day = 0
+    want_number = dict(zip(want, number))
+    # 언제 가입하면 좋을 지 확인
+    for i in range(len(discount) - 10 + 1):
+        # 동일할 경우 증가
+        if want_number == Counter(discount[i:i + 10]):
+            best_day += 1
+    return best_day
+        
