8ee0e6e37a883ac8fa71d43573d5a66bd716a6bd
jongmung <jmp99@naver.com>
2023-11-16 23:17:10 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -1530,3 +1530,13 @@ def solution(phoneBook):
         if p2.startswith(p1):
             return False
     return True
+
+# 모음 사전
+# 사전에 알파벳 모음 'A', 'E', 'I', 'O', 'U'만을 사용하여 만들 수 있는, 길이 5 이하의 모든 단어가 수록되어 있습니다.
+# 사전에서 첫 번째 단어는 "A"이고, 그다음은 "AA"이며, 마지막 단어는 "UUUUU"입니다.
+# 단어 하나 word가 매개변수로 주어질 때, 이 단어가 사전에서 몇 번째 단어인지 return 하도록 solution 함수를 완성해주세요.
+def solution(word):
+    answer = 0
+    for i, n in enumerate(word):
+        answer += (5 ** (5 - i) - 1) / (5 - 1) * "AEIOU".index(n) + 1
+    return answer
