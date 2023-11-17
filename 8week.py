c4fbe2b96742d67ed569e1c222b30f2c0914ec24
jongmung <jmp99@naver.com>
2023-11-17 23:45:15 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -1540,3 +1540,32 @@ def solution(word):
     for i, n in enumerate(word):
         answer += (5 ** (5 - i) - 1) / (5 - 1) * "AEIOU".index(n) + 1
     return answer
+
+# 타겟 넘버
+# n개의 음이 아닌 정수들이 있습니다. 이 정수들을 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다.
+# 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+#   -1+1+1+1+1 = 3
+#   +1-1+1+1+1 = 3
+#   +1+1-1+1+1 = 3
+#   +1+1+1-1+1 = 3
+#   +1+1+1+1-1 = 3
+# 사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때
+# 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
+def solution(numbers, target):
+    leaves = [0]          # 모든 계산 결과를 담자      
+    count = 0 
+    for num in numbers : 
+        temp = []
+	
+    # 자손 노드 
+        for leaf in leaves : 
+            temp.append(leaf + num)    # 더하는 경우 
+            temp.append(leaf - num)    # 빼는 경우 
+        leaves = temp 
+
+  # 모든 경우의 수 계산 후 target과 같은지 확인 
+    for leaf in leaves : 
+        if leaf == target : 
+            count += 1
+
+    return count
