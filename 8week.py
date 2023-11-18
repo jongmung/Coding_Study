22caf4658cb77bc1b8429861cf2f9cf2c904e1ea
jongmung <jmp99@naver.com>
2023-11-18 23:54:12 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -1569,3 +1569,24 @@ def solution(numbers, target):
             count += 1
 
     return count
+
+# 더 맵게
+# 매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다.
+# 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+#   섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 
+#                       + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+# Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+# Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때, 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+import heapq
+def solution(scoville, k):
+    heap = []
+    for num in scoville:
+        heapq.heappush(heap, num)
+    mix_cnt = 0
+    while heap[0] < k:
+        try:
+            heapq.heappush(heap, heapq.heappop(heap) + (heapq.heappop(heap) * 2))
+        except IndexError:
+            return -1
+        mix_cnt += 1
+    return mix_cnt
