fae358611d027b3f0c65010121b2e4f655fbd7c3
jongmung <jmp99@naver.com>
2023-11-22 23:49:39 +0900

    codingst8

--- 8week.py
+++ 8week.py
@@ -1620,3 +1620,30 @@ def solution(numbers):
             answer[stack.pop()]=target
         stack.append(i)
     return answer
+
+# 숫자 변환하기
+# 자연수 x를 y로 변환하려고 합니다. 사용할 수 있는 연산은 다음과 같습니다.
+#   x에 n을 더합니다
+#   x에 2를 곱합니다.
+#   x에 3을 곱합니다.
+# 자연수 x, y, n이 매개변수로 주어질 때,
+# x를 y로 변환하기 위해 필요한 최소 연산 횟수를 return하도록 solution 함수를 완성해주세요. 이때 x를 y로 만들 수 없다면 -1을 return 해주세요.
+def solution(x, y, n):
+    answer = 0
+    dp = set()
+    dp.add(x)
+    while dp:
+        if y in dp:
+            return answer
+        else:
+            dp_y = set()
+            for i in dp:
+                if i+n <= y:
+                    dp_y.add(i+n)
+                if i*2 <= y:
+                    dp_y.add(i*2)
+                if i*3 <= y:
+                    dp_y.add(i*3)
+            dp = dp_y
+            answer += 1
+    return -1
