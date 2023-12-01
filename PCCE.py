0fd2961bf5e691cc53a097e5746916f7cf02df40
jongmung <jmp99@naver.com>
2023-12-01 22:56:54 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -85,3 +85,19 @@ def solution(route):
         elif i == "W":
             east -= 1
     return [east, north]
+
+# [PCCE 기출문제] 6번 / 가채점
+# 성적을 문의하려는 학생들의 번호가 담긴 정수 리스트 numbers와
+# 가채점한 점수가 성적을 문의하려는 학생 순서대로 담긴 정수 리스트 our_score,
+# 실제 성적이 번호 순서대로 담긴 정수 리스트 score_list가 주어집니다.
+# 주어진 solution 함수는 가채점한 점수가 실제 성적과 동일하다면 "Same"을,
+# 다르다면 "Different"를 순서대로 리스트에 담아 return하는 함수입니다.
+# solution 함수가 올바르게 작동하도록 한 줄을 수정해 주세요.
+def solution(numbers, our_score, score_list):
+    answer = []
+    for i in range(len(numbers)):
+        if our_score[i] == score_list[numbers[i]-1]:
+            answer.append("Same")
+        else:
+            answer.append("Different")
+    return answer
