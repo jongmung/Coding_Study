6e6feb3eeb93c69a8f4cee9c5cf6ac570935a027
jongmung <jmp99@naver.com>
2023-12-07 23:58:57 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -235,3 +235,28 @@ def solution(s):
             if j not in answer:
                 answer.append(j)
     return list(map(int,answer))
+
+# k진수에서 소수 개수 구하기
+# 정수 n과 k가 매개변수로 주어집니다.
+# n을 k진수로 바꿨을 때, 변환된 수 안에서 찾을 수 있는
+# 위 조건에 맞는 소수의 개수를 return 하도록 solution 함수를 완성해 주세요.
+def solution(n, k):
+    word=""
+    while n:            # 숫자를 k진법으로 변환
+        word = str(n%k)+word
+        n=n//k 
+    word=word.split("0")  # 변환된 숫자를 0을 기준으로 나눈다.
+    count=0
+    for w in word:
+        if len(w)==0:    # 만약 0또는 1이거나 빈공간이라면 continue를 통해 건너뛴다.
+            continue
+        if int(w)<2:
+            continue
+        sosu=True
+        for i in range(2,int(int(w)**0.5)+1): # 소수찾기
+            if int(w)%i==0:
+                sosu=False
+                break
+        if sosu:
+            count+=1      
+    return count
