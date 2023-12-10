af82027d714958e551528c86444514babd35aeb1
jongmung <jmp99@naver.com>
2023-12-10 23:07:35 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -334,3 +334,20 @@ def solution(fees, records):
         answer.append(money)
 
     return answer
+
+# 땅따먹기
+# 땅따먹기 게임을 하려고 합니다. 땅따먹기 게임의 땅(land)은 총 N행 4열로 이루어져 있고,
+# 모든 칸에는 점수가 쓰여 있습니다. 1행부터 땅을 밟으며 한 행씩 내려올 때,
+# 각 행의 4칸 중 한 칸만 밟으면서 내려와야 합니다.
+# 단, 땅따먹기 게임에는 한 행씩 내려올 때, 같은 열을 연속해서 밟을 수 없는 특수 규칙이 있습니다.
+# 마지막 행까지 모두 내려왔을 때, 얻을 수 있는 점수의 최대값을 return하는 solution 함수를 완성해 주세요.
+# 위 예의 경우,
+# 1행의 네번째 칸 (5), 2행의 세번째 칸 (7), 3행의 첫번째 칸 (4) 땅을 밟아
+# 16점이 최고점이 되므로 16을 return 하면 됩니다.
+def solution(land):
+
+    for i in range(1,len(land)):
+        for j in range(len(land[0])):
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+
+    return max(land[len(land)-1])
