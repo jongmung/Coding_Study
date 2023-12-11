d2b14cee23bddc1c394f54c32143683efea5f9ea
jongmung <jmp99@naver.com>
2023-12-11 23:06:10 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -351,3 +351,32 @@ def solution(land):
             land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
 
     return max(land[len(land)-1])
+
+# 다리를 지나는 트럭
+# 트럭 여러 대가 강을 가로지르는 일차선 다리를 정해진 순으로 건너려 합니다.
+# 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 알아내야 합니다.
+# 다리에는 트럭이 최대 bridge_length대 올라갈 수 있으며,
+# 다리는 weight 이하까지의 무게를 견딜 수 있습니다.
+# 단, 다리에 완전히 오르지 않은 트럭의 무게는 무시합니다.
+# solution 함수의 매개변수로 다리에 올라갈 수 있는 트럭 수 bridge_length,
+# 다리가 견딜 수 있는 무게 weight, 트럭 별 무게 truck_weights가 주어집니다.
+# 이때 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 return 하도록 solution 함수를 완성하세요.
+def solution(bridge_length, weight, truck_weights):
+    
+    answer = 0
+    bridge = [0 for _ in range(bridge_length)]
+    
+    while bridge:
+        
+        answer += 1
+        bridge.pop(0)
+        
+        if truck_weights:
+            if sum(bridge) + truck_weights[0] <= weight:            
+                t = truck_weights.pop(0)
+                bridge.append(t)
+            else:
+                bridge.append(0)
+                 
+         
+    return answer
