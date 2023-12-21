34a305035c331bcaee3c4875af9f785c631be04e
jongmung <jmp99@naver.com>
2023-12-21 23:53:30 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -568,3 +568,21 @@ def solution(numbers):
         
         answer.append(int(''.join(bin_number), 2))
     return answer
+
+# 124 나라의 숫자
+# 124 나라가 있습니다.
+# 124 나라에서는 10진법이 아닌 다음과 같은 자신들만의 규칙으로 수를 표현합니다.
+#   124 나라에는 자연수만 존재합니다.
+#   124 나라에는 모든 수를 표현할 때 1, 2, 4만 사용합니다.
+# 자연수 n이 매개변수로 주어질 때,
+# n을 124 나라에서 사용하는 숫자로 바꾼 값을 return 하도록 solution 함수를 완성해 주세요.
+def solution(n):
+    answer = ''
+    while n:
+        if n % 3:
+            answer += str(n % 3)
+            n //= 3
+        else:
+            answer += "4"
+            n = n//3 - 1
+    return answer[::-1]
