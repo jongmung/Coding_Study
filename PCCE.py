43631e9ba346e3abc5890a1a7588da55840d8222
jongmung <jmp99@naver.com>
2023-12-24 11:41:05 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -613,3 +613,51 @@ def solution(board):
 import math
 def solution(w,h):
     return w*h - (w+h-math.gcd(w,h))
+
+# 과제 진행하기
+# 과제를 받은 루는 다음과 같은 순서대로 과제를 하려고 계획을 세웠습니다.
+#   과제는 시작하기로 한 시각이 되면 시작합니다.
+#   새로운 과제를 시작할 시각이 되었을 때, 기존에 진행 중이던 과제가 있다면 진행 중이던 과제를 멈추고 새로운 과제를 시작합니다.
+#   진행중이던 과제를 끝냈을 때, 잠시 멈춘 과제가 있다면, 멈춰둔 과제를 이어서 진행합니다.
+#   만약, 과제를 끝낸 시각에 새로 시작해야 되는 과제와 잠시 멈춰둔 과제가 모두 있다면, 새로 시작해야 하는 과제부터 진행합니다.
+#   멈춰둔 과제가 여러 개일 경우, 가장 최근에 멈춘 과제부터 시작합니다.
+# 과제 계획을 담은 이차원 문자열 배열 plans가 매개변수로 주어질 때, 과제를 끝낸 순서대로 이름을 배열에 담아 return 하는 solution 함수를 완성해주세요.
+def solution(plans):
+    answer = []
+    for i in range(len(plans)):
+        h, m = map(int, plans[i][1].split(':'))
+        st = h*60+m
+        plans[i][1] = st
+        plans[i][2] = int(plans[i][2])
+        
+    plans.sort(key=lambda x:x[1])
+    stack = []
+    for i in range(len(plans)):
+        if i == len(plans)-1:
+            stack.append(plans[i])
+            break
+        
+        sub, st, t = plans[i]
+        nsub, nst, nt = plans[i+1]
+        if st + t <= nst:
+            answer.append(sub)
+            temp_time = nst - (st+t)
+            
+            while temp_time != 0 and stack:
+                tsub, tst, tt = stack.pop()
+                if temp_time >= tt:
+                    answer.append(tsub)
+                    temp_time -= tt
+                else:
+                    stack.append([tsub, tst, tt - temp_time])
+                    temp_time = 0
+            
+        else:
+            plans[i][2] = t - (nst - st)
+            stack.append(plans[i])
+        
+    while stack:
+        sub, st, tt = stack.pop()
+        answer.append(sub)
+
+    return answer
