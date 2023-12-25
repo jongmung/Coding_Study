95f1d31985548ecfb43233acc018257d986cd03b
jongmung <jmp99@naver.com>
2023-12-25 23:49:04 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -661,3 +661,22 @@ def solution(plans):
         answer.append(sub)
 
     return answer
+
+# 두 원 사이의 정수 쌍
+# x축과 y축으로 이루어진 2차원 직교 좌표계에 중심이 원점인 서로 다른 크기의 원이 두 개 주어집니다.
+# 반지름을 나타내는 두 정수 r1, r2가 매개변수로 주어질 때,
+# 두 원 사이의 공간에 x좌표와 y좌표가 모두 정수인 점의 개수를 return하도록 solution 함수를 완성해주세요.
+# ※ 각 원 위의 점도 포함하여 셉니다.
+import math
+def solution(r1, r2):
+    answer = 0
+    for i in range(1, r2+1):
+        if i < r1 :
+            s = math.ceil(math.sqrt((r1**2 - i**2)))
+        else : 
+            s = 0
+
+        e = int(math.sqrt((r2**2 - i**2)))
+        answer = answer + e - s + 1
+
+    return answer*4
