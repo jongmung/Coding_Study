6be84f97b3cf80665cf32e61c91e686b016af132
jongmung <jmp99@naver.com>
2023-12-26 23:34:18 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -680,3 +680,32 @@ def solution(r1, r2):
         answer = answer + e - s + 1
 
     return answer*4
+
+# 마법의 엘리베이터
+# 마법의 엘리베이터에는 -1, +1, -10, +10, -100, +100 등과 같이
+# 절댓값이 10c (c ≥ 0 인 정수) 형태인 정수들이 적힌 버튼이 있습니다.
+# 마법의 엘리베이터의 버튼을 누르면 현재 층 수에 버튼에 적혀 있는 값을 더한 층으로 이동하게 됩니다.
+# 단, 엘리베이터가 위치해 있는 층과 버튼의 값을 더한 결과가 0보다 작으면 엘리베이터는 움직이지 않습니다.
+# 민수의 세계에서는 0층이 가장 아래층이며 엘리베이터는 현재 민수가 있는 층에 있습니다.
+# 마법의 돌을 아끼기 위해 민수는 항상 최소한의 버튼을 눌러서 이동하려고 합니다.
+# 민수가 어떤 층에서 엘리베이터를 타고 0층으로 내려가는데 필요한 마법의 돌의 최소 개수를 알고 싶습니다.
+# 민수와 마법의 엘리베이터가 있는 층을 나타내는 정수 storey 가 주어졌을 때,
+# 0층으로 가기 위해 필요한 마법의 돌의 최소값을 return 하도록 solution 함수를 완성하세요.
+def solution(storey):
+    answer = 0
+    while storey:
+        remainder = storey % 10
+        # 6 ~ 9
+        if remainder > 5:
+            answer += (10 - remainder)
+            storey += 10
+        # 0 ~ 4
+        elif remainder < 5:
+            answer += remainder
+        # 5
+        else:
+            if (storey // 10) % 10 > 4:
+                storey += 10
+            answer += remainder
+        storey //= 10
+    return answer
