b0c75ed7e18558a234088fe33133e98dd53d9bbe
jongmung <jmp99@naver.com>
2023-12-31 23:50:30 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -773,3 +773,32 @@ def solution(n, wires):
         [s.update(v) for _ in sub for v in sub if set(v) & s]  # 집합연산자 & : 교집합 연산,   집합연산자 update : 여러데이터를 한번에 추가
         ans = min(ans, abs(2 * len(s) - n))
     return ans
+
+# 쿼드압축 후 개수 세기
+# 0과 1로 이루어진 2^ x 2^ 크기의 2차원 정수 배열 arr이 있습니다.
+# 당신은 이 arr을 쿼드 트리와 같은 방식으로 압축하고자 합니다.
+# 구체적인 방식은 다음과 같습니다.
+#   당신이 압축하고자 하는 특정 영역을 S라고 정의합니다.
+#   만약 S 내부에 있는 모든 수가 같은 값이라면, S를 해당 수 하나로 압축시킵니다.
+#   그렇지 않다면, S를 정확히 4개의 균일한 정사각형 영역(입출력 예를 참고해주시기 바랍니다.)으로 쪼갠 뒤,
+# 각 정사각형 영역에 대해 같은 방식의 압축을 시도합니다.
+# arr이 매개변수로 주어집니다. 위와 같은 방식으로 arr을 압축했을 때,
+# 배열에 최종적으로 남는 0의 개수와 1의 개수를 배열에 담아서 return 하도록 solution 함수를 완성해주세요.
+def solution(arr):
+    result=[0,0]
+    length=len(arr)
+    def compression(a,b,l):
+        start=arr[a][b]
+        for i in range(a,a+l):
+            for j in range(b,b+l):
+                if arr[i][j]!=start:
+                    l=l//2
+                    compression(a,b,l)
+                    compression(a,b+l,l)
+                    compression(a+l,b,l)
+                    compression(a+l,b+l,l)
+                    return
+                
+        result[start]+=1   
+    compression(0,0,length)    
+    return (result)
