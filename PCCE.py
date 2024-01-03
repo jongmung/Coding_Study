ad9611bd1eea71ecebcdbfee6584b2d622502d98
jongmung <jmp99@naver.com>
2024-01-03 23:52:19 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -842,3 +842,31 @@ def solution(k, d):
         res_d = math.floor(math.sqrt(d*d - x*x))
         answer += (res_d // k) + 1
     return answer 
+
+# N-Queen
+# 가로, 세로 길이가 n인 정사각형으로된 체스판이 있습니다.
+# 체스판 위의 n개의 퀸이 서로를 공격할 수 없도록 배치하고 싶습니다.
+# 예를 들어서 n이 4인경우 다음과 같이 퀸을 배치하면 n개의 퀸은 서로를 한번에 공격 할 수 없습니다.
+# 체스판의 가로 세로의 세로의 길이 n이 매개변수로 주어질 때,
+# n개의 퀸이 조건에 만족 하도록 배치할 수 있는 방법의 수를 return하는 solution함수를 완성해주세요.
+def solution(n):
+	# 어태까지의 queen 위치 ls, 내가 두려는 위치 new
+    def check(ls, new):
+        for i in range(len(ls)):
+        	# 같은 열에 퀸을 둔 적이 있거나, 대각 위치에 둔 적이 있다면, return False
+            if new == ls[i] or (len(ls)-i) == abs(ls[i]-new):
+                return False
+        return True
+    def dfs(n, ls):
+    	# 끝 행까지 도달! return 1
+        if len(ls) == n:
+            return 1
+        # 끝 행이 아니라면, 다음 줄을 다시 탐색
+        cnt = 0
+        for i in range(n):
+            if check(ls, i):
+                cnt += dfs(n, ls+[i])
+        # 탐색 결과를 return
+        return cnt
+        
+    return dfs(n, [])
