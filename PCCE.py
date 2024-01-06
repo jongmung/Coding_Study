a72bdd6ee18eddfb9d80054e616d3af857a5df36
jongmung <jmp99@naver.com>
2024-01-06 23:59:53 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -946,3 +946,37 @@ def solution(new_id):
     if len(answer) <= 3:
         answer = answer + answer[-1] * (3-len(answer))
     return answer
+
+# 삼각 달팽이
+def solution(n):
+
+    triangle = [ [0] * n for _ in range(n) ]
+    answer = []
+    x, y = -1, 0
+    num = 1
+
+    for i in range(n):
+        for j in range(i, n):
+			
+            # Down
+            if i % 3 == 0:
+                x += 1
+			
+            # Right
+            elif i % 3 == 1:
+                y += 1
+			
+            # Up
+            elif i % 3 == 2:
+                x -= 1
+                y -= 1
+
+            triangle[x][y] = num
+            num += 1
+
+    for i in range(n):
+        for j in range(i+1):
+            answer.append(triangle[i][j])
+
+    return answer
+    
