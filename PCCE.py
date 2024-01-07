6a900dffa0d23476e85112fda5275a4de199efb8
jongmung <jmp99@naver.com>
2024-01-07 23:59:39 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -948,6 +948,7 @@ def solution(new_id):
     return answer
 
 # 삼각 달팽이
+# 
 def solution(n):
     triangle = [ [0] * n for _ in range(n) ]
     answer = []
@@ -978,3 +979,17 @@ def solution(n):
 
     return answer
     
+# 메뉴 리뉴얼
+from itertools import combinations
+from collections import Counter
+def solution(orders, course):
+    answer = []
+    for k in course:
+        candidates = []
+        for menu_li in orders:
+            for li in combinations(menu_li, k):
+                res = ''.join(sorted(li))
+                candidates.append(res)
+        sorted_candidates = Counter(candidates).most_common()
+        answer += [menu for menu, cnt in sorted_candidates if cnt > 1 and cnt == sorted_candidates[0][1]]
+    return sorted(answer)
