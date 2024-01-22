77747723305fc44b6f9abf20bb42e5041cc3ea2b
jongmung <jmp99@naver.com>
2024-01-22 23:33:28 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -1485,3 +1485,29 @@ def solution(n, t, m, p):
         answer += candidate[i]
 
     return answer
+
+# 방문 길이
+# 게임 캐릭터를 4가지 명령어를 통해 움직이려 합니다. 명령어는 다음과 같습니다.
+#   U: 위쪽으로 한 칸 가기
+#   D: 아래쪽으로 한 칸 가기
+#   R: 오른쪽으로 한 칸 가기
+#   L: 왼쪽으로 한 칸 가기
+# 캐릭터는 좌표평면의 (0, 0) 위치에서 시작합니다.
+# 좌표평면의 경계는 왼쪽 위(-5, 5), 왼쪽 아래(-5, -5), 오른쪽 위(5, 5), 오른쪽 아래(5, -5)로 이루어져 있습니다.
+# 이때, 우리는 게임 캐릭터가 지나간 길 중 캐릭터가 처음 걸어본 길의 길이를 구하려고 합니다.
+# 명령어가 매개변수 dirs로 주어질 때, 게임 캐릭터가 처음 걸어본 길의 길이를 구하여 return 하는 solution 함수를 완성해 주세요.
+def solution(dirs):
+    sets = set()
+    y, x = 0, 0
+    udrl = {'U': (1, 0), 'D': (-1, 0), 'R': (0, 1), 'L': (0, -1)}
+    
+    for d in dirs:
+        dy, dx = udrl[d]
+        ny = y + dy
+        nx = x + dx
+        if -5 <= ny <= 5 and -5 <= nx <= 5:
+            sets.add(((y, x), (ny, nx)))
+            sets.add(((ny, nx), (y, x)))
+            y = ny
+            x = nx
+    return len(sets) // 2
