79d5241066fefcb891a6a098444a6ebf0c90e44c
jongmung <jmp99@naver.com>
2024-01-26 23:50:45 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -1615,3 +1615,68 @@ def solution(m, n, board):
                         moved = 1
             if moved == 0:
                 break
+            
+# [3차] 방금그곡
+# 네오는 자신이 기억한 멜로디를 가지고 방금그곡을 이용해 음악을 찾는다.
+# 그런데 라디오 방송에서는 한 음악을 반복해서 재생할 때도 있어서 네오가 기억하고 있는 멜로디는 음악 끝부분과 처음 부분이 이어서 재생된 멜로디일 수도 있다.
+# 반대로, 한 음악을 중간에 끊을 경우 원본 음악에는 네오가 기억한 멜로디가 들어있다 해도 그 곡이 네오가 들은 곡이 아닐 수도 있다.
+# 그렇기 때문에 네오는 기억한 멜로디를 재생 시간과 제공된 악보를 직접 보면서 비교하려고 한다.
+# 다음과 같은 가정을 할 때 네오가 찾으려는 음악의 제목을 구하여라.
+#   방금그곡 서비스에서는 음악 제목, 재생이 시작되고 끝난 시각, 악보를 제공한다.
+#   네오가 기억한 멜로디와 악보에 사용되는 음은 C, C#, D, D#, E, F, F#, G, G#, A, A#, B 12개이다.
+#   각 음은 1분에 1개씩 재생된다. 음악은 반드시 처음부터 재생되며 음악 길이보다 재생된 시간이 길 때는 음악이 끊김 없이 처음부터 반복해서 재생된다.
+#       음악 길이보다 재생된 시간이 짧을 때는 처음부터 재생 시간만큼만 재생된다.
+#   음악이 00:00를 넘겨서까지 재생되는 일은 없다.
+#   조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환한다.
+#       재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
+#   조건이 일치하는 음악이 없을 때에는 “(None)”을 반환한다.
+def change(music):
+    if 'A#' in music:
+        music = music.replace('A#', 'a')
+    if 'F#' in music:
+        music = music.replace('F#', 'f')
+    if 'C#' in music:
+        music = music.replace('C#', 'c')
+    if 'G#' in music:
+        music = music.replace('G#', 'g')
+    if 'D#' in music:
+        music = music.replace('D#', 'd')
+    return music
+
+def solution(m, musicinfos):
+    answer = []
+    index = 0  # 먼저 입력된 음악을 판단하기 위해 index 추가
+    for info in musicinfos:
+        index += 1
+        music = info.split(',')
+        start = music[0].split(':') # 시작 시간
+        end = music[1].split(':')  # 종료 시간
+        # 재생시간 계산
+        time = (int(end[0])*60 + int(end[1])) - (int(start[0])*60 + int(start[1]))
+        
+        # 악보에 #이 붙은 음을 소문자로 변환
+        changed = change(music[3])
+        
+        # 음악 길이
+        a = len(changed)
+        
+        # 재생시간에 재생된 음
+        b = changed * (time // a) + changed[:time%a]
+        
+        # 기억한 멜로디도 #을 제거
+        m = change(m)
+        
+        # 기억한 멜로디가 재생된 음에 있다면 결과배열에 [시간, index, 제목]을 추가
+        if m in b:
+            answer.append([time, index, music[2]])
+    
+    # 결과배열이 비어있다면 "None" 리턴
+    if not answer:
+        return "(None)"
+    # 결과배열의 크기가 1이라면 제목 리턴
+    elif len(answer) == 1:
+        return answer[0][2]
+    # 결과 배열의 크기가 2보다 크다면 재생된 시간이 긴 음악, 먼저 입력된 음악순으로 정렬
+    else:
+        answer = sorted(answer, key=lambda x: (-x[0], x[1]))
+        return answer[0][2] # 첫번째 제목을 리턴
