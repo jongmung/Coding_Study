44b491e37d006c007408dbf9131d185e554c5a25
jongmung <jmp99@naver.com>
2024-02-01 23:52:05 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -1943,3 +1943,46 @@ def solution(k, ranges):
         # 시작점이 끝점보다 커서 유효하지 않은 구간
         else: answer.append(-1)
     return answer
+
+# 교점에 별 만들기
+# Ax + By + C = 0으로 표현할 수 있는 n개의 직선이 주어질 때, 이 직선의 교점 중 정수 좌표에 별을 그리려 합니다.
+# 직선 A, B, C에 대한 정보가 담긴 배열 line이 매개변수로 주어집니다.
+# 이때 모든 별을 포함하는 최소 사각형을 return 하도록 solution 함수를 완성해주세요.
+from itertools import combinations
+def calculation(eq1, eq2):
+    x1, y1, c1 = eq1 # 직선1
+    x2, y2, c2 = eq2 # 직선2
+    
+    # 기울기가 깉아 해가 없는 경우
+    if x1*y2 == y1*x2: 
+        return
+    
+    # 두 직선의 해
+    x = (y1*c2-c1*y2)/(x1*y2-y1*x2)
+    y = (c1*x2-x1*c2)/(x1*y2-y1*x2)
+    
+    # 두 직선의 해 x, y가 모두 정수라면 반환
+    if x == int(x) and y == int(y):
+        return [int(x), int(y)]
+
+def solution(lines):
+    points = []
+    # 모든 선들의 교점 확인
+    for eq1, eq2 in combinations(lines,2):
+        point = calculation(eq1,eq2)
+        if point: points.append(point)
+        
+    # 그림의 시작점과 마지막점 찾기
+    w1, w2 = min(points, key = lambda x : x[0])[0], max(points, key = lambda x : x[0])[0] + 1
+    h1, h2 = min(points, key = lambda x : x[1])[1], max(points, key = lambda x : x[1])[1] + 1
+    
+    # 별을 포함하는 최소한의 크기 배열 생성
+    answer = [['.'] * (w2 - w1) for _ in range((h2 - h1))]
+
+    # 그림의 시작점을 기준으로 교점 위치 "*" 변환
+    for x, y in points:
+        answer[y-h1][x-w1] = '*'
+    
+    answer.reverse()
+    
+    return [''.join(a) for a in answer]
