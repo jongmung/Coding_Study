217df9f36a94a5651f2a876597dc21db02633216
jongmung <jmp99@naver.com>
2024-02-04 23:12:13 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -2073,3 +2073,26 @@ def solution(board):
     if won(board, 'X') and o_count != x_count:
         return 0
     return 1
+
+# 3xN 타일링
+# 가로 길이가 2이고 세로의 길이가 1인 직사각형 모양의 타일이 있습니다.
+# 이 직사각형 타일을 이용하여 세로의 길이가 3이고 가로의 길이가 n인 바닥을 가득 채우려고 합니다.
+# 타일을 채울 때는 다음과 같이 2가지 방법이 있습니다
+#   타일을 가로로 배치 하는 경우
+#   타일을 세로로 배치 하는 경우
+# 직사각형의 가로의 길이 n이 매개변수로 주어질 때, 이 직사각형을 채우는 방법의 수를 return 하는 solution 함수를 완성해주세요.
+def solution(n):
+    mod = 1000000007
+    dp = [0 for i in range(n+1)]
+    dp[2] = 3
+    if n > 2:
+        dp[4] = 11
+        for i in range(6, n+1):
+            if i % 2 == 0:
+                dp[i] = dp[i-2] * 3 + 2
+                for j in range(i-4, -1, -2):
+                    dp[i] += dp[j] * 2
+                dp[i] = dp[i] % mod
+            else:
+                dp[i] = 0
+    return dp[n]
