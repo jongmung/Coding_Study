4beb7cc2ef779841232867794f081e690ecb38db
jongmung <jmp99@naver.com>
2024-02-13 23:34:26 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -2373,3 +2373,36 @@ def solution(wall):
                 a.append(i)
                 b.append(j)
     return [min(a), min(b), max(a) + 1, max(b) + 1]
+
+# 후보키
+# 그의 학부 시절 프로그래밍 경험을 되살려, 모든 인적사항을 데이터베이스에 넣기로 하였고,
+# 이를 위해 정리를 하던 중에 후보키(Candidate Key)에 대한 고민이 필요하게 되었다.
+# 후보키에 대한 내용이 잘 기억나지 않던 제이지는, 정확한 내용을 파악하기 위해 데이터베이스 관련 서적을 확인하여 아래와 같은 내용을 확인하였다.
+#   관계 데이터베이스에서 릴레이션(Relation)의 튜플(Tuple)을 유일하게 식별할 수 있는 속성(Attribute) 또는 속성의 집합 중,
+#   다음 두 성질을 만족하는 것을 후보 키(Candidate Key)라고 한다.
+#       유일성(uniqueness) : 릴레이션에 있는 모든 튜플에 대해 유일하게 식별되어야 한다.
+#       최소성(minimality) : 유일성을 가진 키를 구성하는 속성(Attribute) 중 하나라도 제외하는 경우 유일성이 깨지는 것을 의미한다.
+#           즉, 릴레이션의 모든 튜플을 유일하게 식별하는 데 꼭 필요한 속성들로만 구성되어야 한다.
+# 제이지를 위해, 아래와 같은 학생들의 인적사항이 주어졌을 때, 후보 키의 최대 개수를 구하라.
+# 릴레이션을 나타내는 문자열 배열 relation이 매개변수로 주어질 때,
+# 이 릴레이션에서 후보 키의 개수를 return 하도록 solution 함수를 완성하라.
+def solution(relation):
+    answer_list = list()
+    for i in range(1, 1 << len(relation[0])):
+        tmp_set = set()
+        for j in range(len(relation)):
+            tmp = ''
+            for k in range(len(relation[0])):
+                if i & (1 << k):
+                    tmp += str(relation[j][k])
+            tmp_set.add(tmp)
+
+        if len(tmp_set) == len(relation):
+            not_duplicate = True
+            for num in answer_list:
+                if (num & i) == num:
+                    not_duplicate = False
+                    break
+            if not_duplicate:
+                answer_list.append(i)
+    return len(answer_list)
