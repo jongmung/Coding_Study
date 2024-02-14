7940afc52fd0dbf4f19c631d776692632084b8d5
jongmung <jmp99@naver.com>
2024-02-14 23:24:06 +0900

    codingst8

--- PCCE.py
+++ PCCE.py
@@ -2406,3 +2406,22 @@ def solution(relation):
             if not_duplicate:
                 answer_list.append(i)
     return len(answer_list)
+
+# [PCCE 기출문제] 9번 / 이웃한 칸
+# 각 칸마다 색이 칠해진 2차원 격자 보드판이 있습니다. 그중 한 칸을 골랐을 때,
+# 위, 아래, 왼쪽, 오른쪽 칸 중 같은 색깔로 칠해진 칸의 개수를 구하려고 합니다.
+# 보드의 각 칸에 칠해진 색깔 이름이 담긴 이차원 문자열 리스트 board와
+# 고른 칸의 위치를 나타내는 두 정수 h, w가 주어질 때 board[h][w]와
+# 이웃한 칸들 중 같은 색으로 칠해져 있는 칸의 개수를 return 하도록 solution 함수를 완성해 주세요.
+def solution(board, h, w):
+    dy = [0,-1,0,1]
+    dx = [-1,0,1,0]
+    answer = 0
+    for i in range(4):
+        nh = h + dy[i]
+        nw = w + dx[i]
+        if 0<= nh < len(board) and 0<= nw < len(board[0]):
+            if board[nh][nw] == board[h][w]:
+                answer += 1
+    
+    return answer
