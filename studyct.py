803c32850e9f652ff215784d2e88c9eff99f3b90
jongmung <jmp99@naver.com>
2024-02-16 23:58:04 +0900

    codingst8

---
+++ studyct.py
@@ -0,0 +1,20 @@
+# 이중우선순위큐
+import heapq
+def solution(operations):
+    heap = []
+
+    for i in operations:
+        op1, op2 = i.split()
+        if op1 == 'I':
+            heapq.heappush(heap, int(op2))
+        elif heap:
+            if op2 == "1":
+                heap = heapq.nlargest(len(heap), heap)[1:]
+                heapq.heapify(heap)
+            elif op2 == '-1':
+                heapq.heappop(heap)
+    if heap:
+        min_value = heap[0]
+        return [heapq.nlargest(1, heap)[0], min_value]
+    else:
+        return [0, 0]
