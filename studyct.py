8823b73429f86648c5fd89fec32aa29c4c24786b
jongmung <jmp99@naver.com>
2024-02-19 23:54:01 +0900

    codingst8

--- studyct.py
+++ studyct.py
@@ -76,3 +76,33 @@ def solution(n, computers):
             answer += 1
         
     return answer
+
+# 단어 변환
+# 두 개의 단어 begin, target과 단어의 집합 words가 있습니다. 아래와 같은 규칙을 이용하여 begin에서 target으로 변환하는 가장 짧은 변환 과정을 찾으려고 합니다.
+#   1. 한 번에 한 개의 알파벳만 바꿀 수 있습니다.
+#   2. words에 있는 단어로만 변환할 수 있습니다.
+#       예를 들어 begin이 "hit", target가 "cog", words가 ["hot","dot","dog","lot","log","cog"]라면
+#       "hit" -> "hot" -> "dot" -> "dog" -> "cog"와 같이 4단계를 거쳐 변환할 수 있습니다.
+# 두 개의 단어 begin, target과 단어의 집합 words가 매개변수로 주어질 때,
+# 최소 몇 단계의 과정을 거쳐 begin을 target으로 변환할 수 있는지 return 하도록 solution 함수를 작성해주세요.
+from collections import deque
+def solution(begin, target, words):
+    if target not in words : 
+        return  0
+    return bfs(begin, target, words)
+#최소 단계를 찾아야 하므로 bfs
+def bfs(begin, target, words):
+    queue = deque()
+    queue.append([begin, 0]) #시작 단어와 단계 0으로 초기화
+    while queue:
+        now, step = queue.popleft()
+        if now == target:
+            return step
+        #단어를 모두 체크하면서, 해당 단어가 변경될 수 있는지 체크
+        for word in words:
+            count = 0
+            for i in range(len(now)): #단어의 길이만큼 반복하여
+                if now[i] != word[i]: #단어에 알파벳 한개씩 체크하기
+                    count += 1
+            if count == 1: 
+                queue.append([word, step+1])
