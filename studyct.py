03cff0112fc9507a14b17bac92e551cba0f8c6e6
jongmung <jmp99@naver.com>
2024-02-20 23:53:39 +0900

    codingst8

--- studyct.py
+++ studyct.py
@@ -106,3 +106,23 @@ def bfs(begin, target, words):
                     count += 1
             if count == 1: 
                 queue.append([word, step+1])
+                
+# 등굣길 
+# 계속되는 폭우로 일부 지역이 물에 잠겼습니다. 물에 잠기지 않은 지역을 통해 학교를 가려고 합니다.
+# 집에서 학교까지 가는 길은 m x n 크기의 격자모양으로 나타낼 수 있습니다.
+# 가장 왼쪽 위, 즉 집이 있는 곳의 좌표는 (1, 1)로 나타내고 가장 오른쪽 아래, 즉 학교가 있는 곳의 좌표는 (m, n)으로 나타냅니다.
+# 격자의 크기 m, n과 물이 잠긴 지역의 좌표를 담은 2차원 배열 puddles이 매개변수로 주어집니다.
+# 오른쪽과 아래쪽으로만 움직여 집에서 학교까지 갈 수 있는 최단경로의 개수를 1,000,000,007로 나눈 나머지를 return 하도록 solution 함수를 작성해주세요.
+def solution(m, n, puddles):
+    puddles = [[q,p] for [p,q] in puddles]      # 미리 puddles 좌표 거꾸로
+    dp = [[0] * (m + 1) for i in range(n + 1)]  # dp 초기화
+    dp[1][1] = 1           # 집의 위치(시작위치)
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1 and j == 1: continue 
+            if [i, j] in puddles:    # 웅덩이 위치의 경우 값을 0으로
+                dp[i][j] = 0
+            else:                    # 현재 칸은 왼쪽 칸, 위 칸의 합산!
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+    return dp[n][m]
