21074e2791a50d9d71037eada112f3f07536b57f
jongmung <jmp99@naver.com>
2024-02-23 23:49:41 +0900

    codingst8

--- studyct.py
+++ studyct.py
@@ -169,3 +169,24 @@ def solution(A, B):
             answer += 1
             del B[0]
     return answer
+
+# 단속카메라
+# 고속도로를 이동하는 모든 차량이 고속도로를 이용하면서 단속용 카메라를 한 번은 만나도록 카메라를 설치하려고 합니다.
+# 고속도로를 이동하는 차량의 경로 routes가 매개변수로 주어질 때,
+# 모든 차량이 한 번은 단속용 카메라를 만나도록 하려면 최소 몇 대의 카메라를 설치해야 하는지를 return 하도록 solution 함수를 완성하세요.
+def solution(routes):
+	# 진출지점에 대해서 오름차순 정렬
+    routes.sort(key=lambda x: x[1])
+    # 기준은 제한사항 참조
+    key = -30001
+    # 필요한 카메라 수
+    cnt = 0
+    for route in routes:
+    	# 기준(카메라)보다 진입지점이 뒤에 있으면
+        if route[0] > key:
+        	# 단속이 안되기에 카메라 하나 더 필요
+            cnt += 1
+            # 새로운 기준은 해당 경로의 진출지점(맨끝)
+            key = route[1]
+            
+    return cnt
