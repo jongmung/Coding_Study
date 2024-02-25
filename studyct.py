8006a660dcc6ca40318b78b34f9b5781d65ac4d0
jongmung <jmp99@naver.com>
2024-02-25 23:32:09 +0900

    codingst8

--- studyct.py
+++ studyct.py
@@ -216,3 +216,29 @@ def solution(genres, plays):
         for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
             answer.append(i)
     return answer
+
+# 야근지수
+# 회사원 Demi는 가끔은 야근을 하는데요, 야근을 하면 야근 피로도가 쌓입니다.
+# 야근 피로도는 야근을 시작한 시점에서 남은 일의 작업량을 제곱하여 더한 값입니다.
+# Demi는 N시간 동안 야근 피로도를 최소화하도록 일할 겁니다.
+# Demi가 1시간 동안 작업량 1만큼을 처리할 수 있다고 할 때,
+# 퇴근까지 남은 N 시간과 각 일에 대한 작업량 works에 대해
+# 야근 피로도를 최소화한 값을 리턴하는 함수 solution을 완성해주세요.
+import heapq
+def solution(n, works):
+    if sum(works) <= n:
+        return 0
+    # 최대힙으로 만듬
+    works = [-w for w in works]
+    heapq.heapify(works)
+    
+    # n번 만큼만 works내 배열의 값을 하향평준화
+    while n>0:
+        val = heapq.heappop(works)
+        val += 1
+        heapq.heappush(works,val)
+        n-=1
+    
+    answer = [ w**2 for w in works ]
+    
+    return sum(answer)
