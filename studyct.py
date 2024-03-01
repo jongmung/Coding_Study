fa51ee3edfda185727c49e68bde0855fd58a19bc
jongmung <jmp99@naver.com>
2024-03-01 23:59:04 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -373,3 +373,25 @@ def solution(n, times):
             left = mid + 1
             
     return answer
+
+# 섬 연결하기
+# n개의 섬 사이에 다리를 건설하는 비용(costs)이 주어질 때,
+# 최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한 최소 비용을 return 하도록 solution을 완성하세요.
+# 다리를 여러 번 건너더라도, 도달할 수만 있으면 통행 가능하다고 봅니다.
+# 예를 들어 A 섬과 B 섬 사이에 다리가 있고, B 섬과 C 섬 사이에 다리가 있으면 A 섬과 C 섬은 서로 통행 가능합니다.
+def solution(n, costs):
+    answer = 0
+    costs.sort(key = lambda x: x[2]) 
+    link = set([costs[0][0]])
+
+    # Kruskal 알고리즘으로 최소 비용 구하기
+    while len(link) != n:
+        for v in costs:
+            if v[0] in link and v[1] in link:
+                continue
+            if v[0] in link or v[1] in link:
+                link.update([v[0], v[1]])
+                answer += v[2]
+                break
+                
+    return answer
