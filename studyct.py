c7c2e29df6972cab86103653cc7381731355f338
jongmung <jmp99@naver.com>
2024-03-04 20:22:48 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -452,3 +452,33 @@ def solution(stones, k):
             s = mid + 1
             answer = mid+1
     return answer
+
+# 연속 펄스 부분 수열의 합
+# 어떤 수열의 연속 부분 수열에 같은 길이의 펄스 수열을 각 원소끼리 곱하여 연속 펄스 부분 수열을 만들려 합니다.
+# 펄스 수열이란 [1, -1, 1, -1 …] 또는 [-1, 1, -1, 1 …] 과 같이 1 또는 -1로 시작하면서 1과 -1이 번갈아 나오는 수열입니다.
+#   예를 들어 수열 [2, 3, -6, 1, 3, -1, 2, 4]의 연속 부분 수열 [3, -6, 1]에 펄스 수열 [1, -1, 1]을 곱하면 연속 펄스 부분수열은 [3, 6, 1]이 됩니다.
+#   또 다른 예시로 연속 부분 수열 [3, -1, 2, 4]에 펄스 수열 [-1, 1, -1, 1]을 곱하면 연속 펄스 부분수열은 [-3, -1, -2, 4]이 됩니다.
+# 정수 수열 sequence가 매개변수로 주어질 때, 연속 펄스 부분 수열의 합 중 가장 큰 것을 return 하도록 solution 함수를 완성해주세요.
+from sys import maxsize
+INF = maxsize
+def solution(sequence):
+    answer = -INF
+    size = len(sequence)
+    s1 = s2 = 0				# 1
+    s1_min = s2_min = 0		# 2
+    pulse = 1
+    
+    for i in range(size):
+        s1 += sequence[i] * pulse
+        s2 += sequence[i] * (-pulse)
+        
+        # 3
+        answer = max(answer, s1-s1_min, s2-s2_min)
+        
+        # 4
+        s1_min = min(s1_min, s1)
+        s2_min = min(s2_min, s2)
+        
+        # 5
+        pulse *= -1
+    return answer
