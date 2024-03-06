ad0130765dfbca86e61fd30f2971d9b44d1a1958
jongmung <jmp99@naver.com>
2024-03-06 22:36:27 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -500,3 +500,44 @@ def solution(n, money):
         for price in range(typ, n+1) :
             dp[price] += dp[price - typ] 
     return dp[-1] % 1000000007
+
+# 부대복귀
+# 강철부대의 각 부대원이 여러 지역에 뿔뿔이 흩어져 특수 임무를 수행 중입니다.
+# 지도에서 강철부대가 위치한 지역을 포함한 각 지역은 유일한 번호로 구분되며,
+# 두 지역 간의 길을 통과하는 데 걸리는 시간은 모두 1로 동일합니다.
+# 임무를 수행한 각 부대원은 지도 정보를 이용하여 최단시간에 부대로 복귀하고자 합니다.
+# 다만 적군의 방해로 인해, 임무의 시작 때와 다르게 되돌아오는 경로가 없어져 복귀가 불가능한 부대원도 있을 수 있습니다.
+# 강철부대가 위치한 지역을 포함한 총지역의 수 n, 두 지역을 왕복할 수 있는 길 정보를 담은 2차원 정수 배열 roads,
+# 각 부대원이 위치한 서로 다른 지역들을 나타내는 정수 배열 sources, 강철부대의 지역 destination이 주어졌을 때,
+# 주어진 sources의 원소 순서대로 강철부대로 복귀할 수 있는 최단시간을 담은 배열을 return하는 solution 함수를 완성해주세요.
+# 복귀가 불가능한 경우 해당 부대원의 최단시간은 -1입니다.
+from collections import deque, defaultdict
+def solution(n, roads, sources, destination):
+    answer = []
+    
+    # n = 총지역 수, roads = 길 정보
+    # sources = 부대원 위치, destination = 강철부대 지역
+    graph = [[] for _ in range(n+1)]
+    for road in roads:
+        a, b = road
+        graph[a].append(b)
+        graph[b].append(a)
+
+    sub_answer = [-1]*(n+1)
+    
+    # bfs 수행
+    q = deque()
+    q.append((destination, 0)) 
+    sub_answer[destination] = 0
+    while q:
+        now, level = q.popleft()
+        
+        for next_v in graph[now]:
+            if sub_answer[next_v] == -1:
+                q.append((next_v, level+1))
+                sub_answer[next_v] = level+1
+
+    for source in sources:
+        answer.append(sub_answer[source])
+    
+    return answer
