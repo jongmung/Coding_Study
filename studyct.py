3892bcede70ac0a1a6e0a7862c36796d857206bc
jongmung <jmp99@naver.com>
2024-03-07 23:58:52 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -541,3 +541,30 @@ def solution(n, roads, sources, destination):
         answer.append(sub_answer[source])
     
     return answer
+
+# 순위
+# n명의 권투선수가 권투 대회에 참여했고 각각 1번부터 n번까지 번호를 받았습니다. 권투 경기는 1대1 방식으로 진행이 되고,
+# 만약 A 선수가 B 선수보다 실력이 좋다면 A 선수는 B 선수를 항상 이깁니다.
+# 심판은 주어진 경기 결과를 가지고 선수들의 순위를 매기려 합니다. 하지만 몇몇 경기 결과를 분실하여 정확하게 순위를 매길 수 없습니다.
+# 선수의 수 n, 경기 결과를 담은 2차원 배열 results가 매개변수로 주어질 때
+# 정확하게 순위를 매길 수 있는 선수의 수를 return 하도록 solution 함수를 작성해주세요.
+def solution(n, results):
+    answer = 0
+    board = [[0]*n for _ in range(n)]
+    
+    for a,b in results:
+        board[a-1][b-1] = 1
+        board[b-1][a-1] = -1
+        
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if i == j or board[i][j] in [1,-1]:
+                    continue
+                if board[i][k] == board[k][j] == 1:
+                    board[i][j] = 1
+                    board[j][i] = board[k][i] = board[j][k] = -1
+    for row in board:
+        if row.count(0) == 1:
+            answer += 1
+    return answer
