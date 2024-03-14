8275c4831c197f8db6d69b04715debae23ece1f4
jongmung <jmp99@naver.com>
2024-03-14 15:16:37 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -778,3 +778,29 @@ def solution(n, t, m, timetable):
             answer = timetable[i-1]-1
             
     return str(answer//60).zfill(2)+":"+str(answer%60).zfill(2)
+
+# 기지국 설치
+# N개의 아파트가 일렬로 쭉 늘어서 있습니다.
+# 이 중에서 일부 아파트 옥상에는 4g 기지국이 설치되어 있습니다.
+# 기술이 발전해 5g 수요가 높아져 4g 기지국을 5g 기지국으로 바꾸려 합니다.
+# 그런데 5g 기지국은 4g 기지국보다 전달 범위가 좁아,
+# 4g 기지국을 5g 기지국으로 바꾸면 어떤 아파트에는 전파가 도달하지 않습니다.
+# 아파트의 개수 N, 현재 기지국이 설치된 아파트의 번호가 담긴 1차원 배열 stations,
+# 전파의 도달 거리 W가 매개변수로 주어질 때,
+# 모든 아파트에 전파를 전달하기 위해 증설해야 할 기지국 개수의 최솟값을 리턴하는 solution 함수를 완성해주세요
+import math
+def solution(n, stations, w):
+    answer = 0
+    dist = []  # 전파 전달 안되는 구간 길이 저장할 리스트
+    for i in range(1, len(stations)):
+        dist.append((stations[i]-w-1)-(stations[i-1]+w))
+    
+    dist.append(stations[0]-w-1)  # 맨앞
+    dist.append(n-(stations[-1]+w))  # 맨뒤
+    
+    for i in dist:
+        if i <= 0:
+            continue
+        else:
+            answer += math.ceil(i/(2*w+1))  # 올림
+    return answer
