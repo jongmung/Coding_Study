eab53a1b8cfeb94f56567cef2102500771dafae2
jongmung <jmp99@naver.com>
2024-03-16 01:21:30 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -804,3 +804,37 @@ def solution(n, stations, w):
         else:
             answer += math.ceil(i/(2*w+1))  # 올림
     return answer
+
+# 선입 선출 스케줄링
+# 처리해야 할 동일한 작업이 n 개가 있고, 이를 처리하기 위한 CPU가 있습니다.
+#   이 CPU는 다음과 같은 특징이 있습니다.
+#   CPU에는 여러 개의 코어가 있고, 코어별로 한 작업을 처리하는 시간이 다릅니다.
+#   한 코어에서 작업이 끝나면 작업이 없는 코어가 바로 다음 작업을 수행합니다.
+#   2개 이상의 코어가 남을 경우 앞의 코어부터 작업을 처리 합니다.
+# 처리해야 될 작업의 개수 n과, 각 코어의 처리시간이 담긴 배열 cores 가 매개변수로 주어질 때,
+# 마지막 작업을 처리하는 코어의 번호를 return 하는 solution 함수를 완성해주세요.
+def solution(n, cores):
+    if n <= len(cores):
+        return n
+    else:
+        n -= len(cores)
+        left = 1
+        right = max(cores) * n
+        while left < right:
+            mid = (left + right) // 2
+            capacity = 0
+            for c in cores:
+                capacity += mid // c
+            if capacity >= n:
+                right = mid
+            else:
+                left = mid + 1
+
+        for c in cores:
+            n -= (right-1) // c
+
+        for i in range(len(cores)):
+            if right % cores[i] == 0:
+                n -= 1
+                if n == 0:
+                    return i + 1
