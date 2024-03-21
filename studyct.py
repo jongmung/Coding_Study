a70882f39c0189ffd5441e8de7d763a558ad97a1
jongmung <jmp99@naver.com>
2024-03-21 23:57:38 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -1008,3 +1008,54 @@ def solution(a, edges):
                     a[node] = 0
 
     return answer if a[0] == 0 else -1
+
+# 도넛과 막대 그래프
+# 도넛 모양 그래프, 막대 모양 그래프, 8자 모양 그래프들이 있습니다. 이 그래프들은 1개 이상의 정점과, 정점들을 연결하는 단방향 간선으로 이루어져 있습니다.
+#   크기가 n인 도넛 모양 그래프는 n개의 정점과 n개의 간선이 있습니다.
+#       도넛 모양 그래프의 아무 한 정점에서 출발해 이용한 적 없는 간선을 계속 따라가면
+#       나머지 n-1개의 정점들을 한 번씩 방문한 뒤 원래 출발했던 정점으로 돌아오게 됩니다. 도넛 모양 그래프의 형태는 다음과 같습니다.
+#   크기가 n인 막대 모양 그래프는 n개의 정점과 n-1개의 간선이 있습니다.
+#       막대 모양 그래프는 임의의 한 정점에서 출발해 간선을 계속 따라가면 나머지 n-1개의 정점을 한 번씩 방문하게 되는 정점이 단 하나 존재합니다.
+#       막대 모양 그래프의 형태는 다음과 같습니다.
+#   크기가 n인 8자 모양 그래프는 2n+1개의 정점과 2n+2개의 간선이 있습니다.
+#       8자 모양 그래프는 크기가 동일한 2개의 도넛 모양 그래프에서 정점을 하나씩 골라 결합시킨 형태의 그래프입니다. 8자 모양 그래프의 형태는 다음과 같습니다.
+# 도넛 모양 그래프, 막대 모양 그래프, 8자 모양 그래프가 여러 개 있습니다. 이 그래프들과 무관한 정점을 하나 생성한 뒤,
+# 각 도넛 모양 그래프, 막대 모양 그래프, 8자 모양 그래프의 임의의 정점 하나로 향하는 간선들을 연결했습니다.
+# 그 후 각 정점에 서로 다른 번호를 매겼습니다.
+# 이때 당신은 그래프의 간선 정보가 주어지면 생성한 정점의 번호와 정점을 생성하기 전
+# 도넛 모양 그래프의 수, 막대 모양 그래프의 수, 8자 모양 그래프의 수를 구해야 합니다.
+# 그래프의 간선 정보를 담은 2차원 정수 배열 edges가 매개변수로 주어집니다.
+# 이때, 생성한 정점의 번호, 도넛 모양 그래프의 수, 막대 모양 그래프의 수,
+# 8자 모양 그래프의 수를 순서대로 1차원 정수 배열에 담아 return 하도록 solution 함수를 완성해 주세요.
+def solution(edges):  
+    def count_edges(edges):
+        edge_counts = {}
+        for a, b in edges:
+            # 각 노드별로 간선의 수를 추가할 딕셔너리를 생성 - .get() 함수를 이용해 딕셔너리의 키 값 추가
+            if not edge_counts.get(a):
+                edge_counts[a] = [0, 0]
+            if not edge_counts.get(b):
+                edge_counts[b] = [0, 0]
+            # output edge와 input edge의 개수를 추가
+            edge_counts[a][0] += 1  # a는 n번 노드에서 나가는 간선
+            edge_counts[b][1] += 1  # b는 n번 노드로 들어오는 간선
+        return edge_counts
+
+    def check_answer(egde_counts):
+        answer = [0, 0, 0, 0]
+        for key, counts in edge_counts.items():
+            # 생성된 정점의 번호 확인
+            if counts[0] >= 2 and counts[1] == 0:
+                answer[0] = key
+            # 막대 모양 그래프의 수 확인
+            elif counts[0] == 0 and counts[1] > 0:
+                answer[2] += 1
+            # 8자 모양 그래프의 수 확인
+            elif counts[0] >= 2 and counts[1] >= 2:
+                answer[3] += 1
+        # 도넛 모양 그래프의 수 확인
+        answer[1] = (edge_counts[answer[0]][0] - answer[2] - answer[3])
+        return answer
+    edge_counts = count_edges(edges)
+    answer = check_answer(edge_counts)
+    return answer
