1d354d7370a9174d58308b0dbb9b5bbf8491c61b
jongmung <jmp99@naver.com>
2024-03-25 23:49:32 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -1134,3 +1134,43 @@ def solution(jobs):
         count.append(now-y)
 
     return sum(count)//num
+
+# 불량 사용자
+# 개발팀 내에서 이벤트 개발을 담당하고 있는 "무지"는 최근 진행된 카카오이모티콘 이벤트에 비정상적인 방법으로 당첨을 시도한 응모자들을 발견하였습니다.
+# 이런 응모자들을 따로 모아 불량 사용자라는 이름으로 목록을 만들어서 당첨 처리 시 제외하도록 이벤트 당첨자 담당자인 "프로도" 에게 전달하려고 합니다.
+# 이 때 개인정보 보호을 위해 사용자 아이디 중 일부 문자를 '*' 문자로 가려서 전달했습니다.
+# 가리고자 하는 문자 하나에 '*' 문자 하나를 사용하였고 아이디 당 최소 하나 이상의 '*' 문자를 사용하였습니다.
+# "무지"와 "프로도"는 불량 사용자 목록에 매핑된 응모자 아이디를 제재 아이디 라고 부르기로 하였습니다.
+# 이벤트 응모자 아이디 목록이 담긴 배열 user_id와 불량 사용자 아이디 목록이 담긴 배열 banned_id가 매개변수로 주어질 때,
+# 당첨에서 제외되어야 할 제재 아이디 목록은 몇가지 경우의 수가 가능한 지 return 하도록 solution 함수를 완성해주세요.
+from itertools import permutations
+from collections import Counter
+def is_match(uid, bid):
+    """ 아이디가 banned_id에 매칭되는지 여부를 리턴 """
+    if len(uid) != len(bid):
+        return False
+    for u, b in zip(uid, bid):
+        if b=="*" or u==b:
+            continue
+        else:
+            return False
+    return True
+
+def is_available_case(user_id, banned_id):
+    """ user_id리스트와 banned_id 리스트 (동일한 길이) 가 매칭되는지 여부를 리턴 """
+    for uid, bid in zip(user_id, banned_id):
+        if not is_match(uid, bid):
+            return False
+    return True
+
+def solution(user_id, banned_id):
+    answer = []
+    # 순열을 통해 user_id에서 banned_id의 길이만큼 샘플링한 리스트에 대하여
+    for u_ids in permutations(user_id, len(banned_id)):
+        # 두 리스트가 매칭되는 경우
+        if is_available_case(u_ids, banned_id):
+            cnt = Counter(u_ids)
+            # 중복되지 않는다면 answer에 추가한다.
+            if cnt not in answer:
+                answer.append(cnt)
+    return len(answer)
