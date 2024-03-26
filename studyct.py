510397fe2cf654b2b46f8d8ddf04798a851a7980
jongmung <jmp99@naver.com>
2024-03-26 23:44:11 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -1174,3 +1174,78 @@ def solution(user_id, banned_id):
             if cnt not in answer:
                 answer.append(cnt)
     return len(answer)
+
+# 자물쇠와 열쇠
+# 잠겨있는 자물쇠는 격자 한 칸의 크기가 1 x 1인 N x N 크기의 정사각 격자 형태이고
+# 특이한 모양의 열쇠는 M x M 크기인 정사각 격자 형태로 되어 있습니다.
+# 자물쇠에는 홈이 파여 있고 열쇠 또한 홈과 돌기 부분이 있습니다.
+# 열쇠는 회전과 이동이 가능하며 열쇠의 돌기 부분을 자물쇠의 홈 부분에 딱 맞게 채우면 자물쇠가 열리게 되는 구조입니다.
+# 자물쇠 영역을 벗어난 부분에 있는 열쇠의 홈과 돌기는 자물쇠를 여는 데 영향을 주지 않지만,
+# 자물쇠 영역 내에서는 열쇠의 돌기 부분과 자물쇠의 홈 부분이 정확히 일치해야 하며 열쇠의 돌기와 자물쇠의 돌기가 만나서는 안됩니다.
+# 또한 자물쇠의 모든 홈을 채워 비어있는 곳이 없어야 자물쇠를 열 수 있습니다.
+# 열쇠를 나타내는 2차원 배열 key와 자물쇠를 나타내는 2차원 배열 lock이 매개변수로 주어질 때,
+# 열쇠로 자물쇠를 열수 있으면 true를, 열 수 없으면 false를 return 하도록 solution 함수를 완성해주세요.
+# NxN 2차원 리스트 d도 회전
+# 회전 각도 d => 1: 90도, 2: 180도, 3: 270도
+def rotate(array, d):
+    n = len(array)  # 배열의 길이
+    result = [[0] * n for _ in range(n)]
+
+    if d % 4 == 1:
+        for r in range(n):
+            for c in range(n):
+                result[c][n - r - 1] = array[r][c]
+    elif d % 4 == 2:
+        for r in range(n):
+            for c in range(n):
+                result[n - r - 1][n - c - 1] = array[r][c]
+    elif d % 4 == 3:
+        for r in range(n):
+            for c in range(n):
+                result[n - c - 1][r] = array[r][c]
+    else:
+        for r in range(n):
+            for c in range(n):
+                result[r][c] = array[r][c]
+
+    return result
+
+
+# 자물쇠 중간 NxN 부분이 모두 1인지 확인
+def check(new_lock):
+    n = len(new_lock) // 3
+    for i in range(n, n * 2):
+        for j in range(n, n * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+
+def solution(key, lock):
+    m = len(key)
+    n = len(lock)
+    # 기존 자물쇠보다 3배 큰 자물쇠
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    # 새로운 자물쇠의 중앙 부분에 기존 자물쇠 넣기
+    for i in range(n):
+        for j in range(n):
+            new_lock[n + i][n + j] = lock[i][j]
+
+    # 열쇠를 (1, 1)부터 (N*2, N*2)까지 이동시키며 확인
+    for i in range(1, n * 2):
+        for j in range(1, n * 2):
+            # 열쇠를 0, 90, 180, 270도로 회전시키며 확인
+            for d in range(4):
+                r_key = rotate(key, d)  # key를 d만큼 회전시킨 리스트
+                for x in range(m):
+                    for y in range(m):
+                        new_lock[i + x][j + y] += r_key[x][y]
+
+                if check(new_lock):
+                    return True
+
+                for x in range(m):
+                    for y in range(m):
+                        new_lock[i + x][j + y] -= r_key[x][y]
+
+    return False
