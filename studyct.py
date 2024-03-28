49fbaecbf086677a1c61611c0b201d32345dcae7
jongmung <jmp99@naver.com>
2024-03-28 19:14:44 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -1272,3 +1272,41 @@ def solution(sticker):
         
     answer = max(dp1[-1], dp2[-1])
     return answer
+
+# 보석 쇼핑
+# 어느 날 스트레스를 풀기 위해 보석 매장에 쇼핑을 하러 간 어피치는 이전처럼 진열대의 특정 범위의 보석을 모두 구매하되 특별히 아래 목적을 달성하고 싶었습니다.
+#   진열된 모든 종류의 보석을 적어도 1개 이상 포함하는 가장 짧은 구간을 찾아서 구매
+# 진열대 번호 순서대로 보석들의 이름이 저장된 배열 gems가 매개변수로 주어집니다.
+# 이때 모든 보석을 하나 이상 포함하는 가장 짧은 구간을 찾아서 return 하도록 solution 함수를 완성해주세요.
+# 가장 짧은 구간의 시작 진열대 번호와 끝 진열대 번호를 차례대로 배열에 담아서 return 하도록 하며,
+# 만약 가장 짧은 구간이 여러 개라면 시작 진열대 번호가 가장 작은 구간을 return 합니다.
+def solution(gems):
+    answer = [0, len(gems)]
+    size = len(set(gems))   # 보석 종류 갯수
+    left, right = 0, 0 # left는 보석 빼 줄 포인터, right는 보석 더해 줄 포인터
+    gem_dict = {gems[0] : 1}
+    
+    while left < len(gems) and right < len(gems):   # 투 포인터가 범위를 벗어나면 무한루프 종료
+        # 딕셔너리에 보석 종류가 다 들어오는 경우
+        if len(gem_dict) == size:
+            if right - left < answer[1] - answer[0]:    # 최소 크기 확인
+                answer = [left, right]       
+            else:
+                gem_dict[gems[left]] -= 1
+                if gem_dict[gems[left]] == 0:
+                    del gem_dict[gems[left]]    # count가 0이 되면 key가 없어야하므로 반드시 del
+                left += 1
+                
+        else:
+            right += 1
+            
+            if right == len(gems):
+                break
+                
+            if gems[right] in gem_dict: # 딕셔너리 key에 있으면 count
+                gem_dict[gems[right]] += 1
+                
+            else:   # 없으면 추가
+                gem_dict[gems[right]] = 1
+    
+    return [answer[0]+1, answer[1]+1] # 시작 인덱스가 1번 진열대 부터 라서 1 증가
