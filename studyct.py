3e8f7e9ee11f5264ae413b3941dbac9f6c3c5402
jongmung <jmp99@naver.com>
2024-04-02 23:59:54 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -1508,3 +1508,36 @@ def solution(n, build_frame):
                 answer.append([x, y, 1])
 
     return answer
+
+# 순위검색
+# 지원자가 지원서에 입력한 4가지의 정보와 획득한 코딩테스트 점수를 하나의 문자열로 구성한 값의 배열 info,
+# 개발팀이 궁금해하는 문의조건이 문자열 형태로 담긴 배열 query가 매개변수로 주어질 때,
+# 각 문의조건에 해당하는 사람들의 숫자를 순서대로 배열에 담아 return 하도록 solution 함수를 완성해 주세요.
+from itertools import combinations
+
+
+def solution(info, query):
+    answer = [0 for i in range(len(query))]
+    infol = []
+    queryl = []
+    for i in range(len(info)):
+        infol.append(info[i].split())
+        # tmp = ['java', 'backend', 'junior', 'pizza', '150']
+    for j in range(len(query)):
+        queryl.append(query[j].split())
+        while len(queryl[j]) > 5:
+            queryl[j].remove("and")
+
+    for i in range(len(infol)):
+        for j in range(len(queryl)):
+            flag = 0
+            for k in range(4):
+                if infol[i][k] != queryl[j][k]:
+                    if queryl[j][k] != '-':
+                        flag = 1
+                        break
+            if flag == 0:
+                if int(infol[i][4]) >= int(queryl[j][4]):
+                    answer[j] += 1
+
+    return answer
