feff11d5516b978007ad07bd343f51c851db1b39
jongmung <jmp99@naver.com>
2024-04-04 23:50:17 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -1559,3 +1559,50 @@ def solution(info, query):
             answer.append(0)
 
     return answer
+
+# 미로 탈출 명령어
+# n x m 격자 미로가 주어집니다. 당신은 미로의 (x, y)에서 출발해 (r, c)로 이동해서 탈출해야 합니다.
+# 단, 미로를 탈출하는 조건이 세 가지 있습니다.
+#   격자의 바깥으로는 나갈 수 없습니다.
+#   (x, y)에서 (r, c)까지 이동하는 거리가 총 k여야 합니다. 이때, (x, y)와 (r, c)격자를 포함해, 같은 격자를 두 번 이상 방문해도 됩니다.
+#   미로에서 탈출한 경로를 문자열로 나타냈을 때, 문자열이 사전 순으로 가장 빠른 경로로 탈출해야 합니다.
+# 이동 경로는 다음과 같이 문자열로 바꿀 수 있습니다.
+#   l: 왼쪽으로 한 칸 이동
+#   r: 오른쪽으로 한 칸 이동
+#   u: 위쪽으로 한 칸 이동
+#   d: 아래쪽으로 한 칸 이동
+# 격자의 크기를 뜻하는 정수 n, m, 출발 위치를 뜻하는 정수 x, y, 탈출 지점을 뜻하는 정수 r, c,
+# 탈출까지 이동해야 하는 거리를 뜻하는 정수 k가 매개변수로 주어집니다.
+# 이때, 미로를 탈출하기 위한 경로를 return 하도록 solution 함수를 완성해주세요.
+# 단, 위 조건대로 미로를 탈출할 수 없는 경우 "impossible"을 return 해야 합니다.
+from collections import deque
+def solution(n, m, x, y, r, c, k):
+    answer = ''
+    # 남은 거리 탐색 자주 해주어야 하므로 함수로 빼주기
+    def manhattan(x1, y1):
+        return abs(x1 - (r-1)) + abs(y1-(c-1))
+
+    # k가 최단 거리보다 작거나, 최단 거리 - k가 홀수라면 도착지에 k번만에 도착 불가
+    if manhattan(x-1, y-1) > k or (manhattan(x-1, y-1) - k) % 2:
+        return 'impossible'
+    # 탐색 방향 사전순으로 - d l r u
+    direct = {(1,0):'d', (0,-1):'l', (0,1):'r', (-1,0):'u'}
+    q = deque()
+    q.append((x-1, y-1, 0, ''))
+    while q:
+        si, sj, cnt, route = q.popleft()
+        # 도착했는데 남은 거리가 홀수라면 도착지에 k만큼 오지 못한다!
+        if (si, sj) == (r-1, c-1) and (k-cnt) % 2:
+            return 'impossible'
+        elif (si, sj) == (r-1, c-1) and cnt == k:
+            return route
+        for di, dj in direct:
+            ni, nj = si+di, sj+dj
+            if 0<=ni<n and 0<=nj<m:
+                # 다음 이동 자리를 보는 것이므로 +1 을 해주어야 함
+                if manhattan(ni, nj) + cnt + 1 > k:
+                    continue
+                q.append((ni, nj, cnt+1, route+direct[(di, dj)]))
+                break
+
+    return answer
