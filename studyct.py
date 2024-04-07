6e11453e25e61a4442839395c172a7dacdbc54f9
jongmung <jmp99@naver.com>
2024-04-07 23:59:31 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -1700,3 +1700,54 @@ def solution(cap, n, deliveries, pickups):
         pick -= pickups[i]
         answer += (i + 1) * cnt
     return answer * 2
+
+# 가장 많이 받은 선물
+# 선물을 직접 전하기 힘들 때 카카오톡 선물하기 기능을 이용해 축하 선물을 보낼 수 있습니다.
+# 당신의 친구들이 이번 달까지 선물을 주고받은 기록을 바탕으로 다음 달에 누가 선물을 많이 받을지 예측하려고 합니다.
+#   두 사람이 선물을 주고받은 기록이 있다면, 이번 달까지 두 사람 사이에 더 많은 선물을 준 사람이 다음 달에 선물을 하나 받습니다.
+#   두 사람이 선물을 주고받은 기록이 하나도 없거나 주고받은 수가 같다면, 선물 지수가 더 큰 사람이 선물 지수가 더 작은 사람에게 선물을 하나 받습니다.
+# 위에서 설명한 규칙대로 다음 달에 선물을 주고받을 때, 당신은 선물을 가장 많이 받을 친구가 받을 선물의 수를 알고 싶습니다.
+# 친구들의 이름을 담은 1차원 문자열 배열 friends 이번 달까지 친구들이 주고받은 선물 기록을 담은 1차원 문자열 배열 gifts가 매개변수로 주어집니다.
+# 이때, 다음달에 가장 많은 선물을 받는 친구가 받을 선물의 수를 return 하도록 solution 함수를 완성해 주세요.
+def solution(friends, gifts):
+    gifted = {} # gifted = {"friend 이름": {"선물 준 친구 이름": 이 친구에게 준 선물 개수}}
+    gift_idx = {} # 선물 지수 
+    # 딕셔너리 초기화
+    for friend in friends:
+        gifted[friend] = {}
+        gift_idx[friend] = 0
+    
+    for gift in gifts:
+        t, f = gift.split(' ') # t: 선물을 준 사람, f: 받은 사람
+        if f in gifted[t]:
+            gifted[t][f] += 1
+        else:
+            gifted[t][f] = 1
+        # 선물 지수 반영
+        gift_idx[t] += 1
+        gift_idx[f] -= 1
+    
+    # 각자 받게 될 선물 개수
+    will_get = [0 for _ in friends] # friends 리스트 순서대로 저장
+    for i in range(len(friends)):
+        curr = friends[i] # 인덱스 i에 해당하는 친구
+        for j in range(i+1, len(friends)):
+            another = friends[j] # 인덱스 j에 해당하는 친구
+            # curr가 another에게 준 선물 개수
+            a = gifted[curr][another] if another in gifted[curr] else 0 
+            # another가 curr에게 준 선물 개수
+            b = gifted[another][curr] if curr in gifted[another] else 0 
+            
+            if a > b: # curr가 선물을 더 많이 줬다면
+                will_get[i] += 1
+            elif a < b: # another가 선물을 더 많이 줬다면
+                will_get[j] += 1
+            elif a == b: # 둘이 선물을 주고 받은 개수가 같다면 선물 지수 확인
+                ai, bi = gift_idx[curr], gift_idx[another]
+                if ai > bi:
+                    will_get[i] += 1
+                elif ai < bi:
+                    will_get[j] += 1
+    
+    answer = max(will_get)
+    return answer
