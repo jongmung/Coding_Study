62129b7be43733775c79347adf90b1e9f200434d
jongmung <jmp99@naver.com>
2024-04-12 23:04:52 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -1924,3 +1924,65 @@ def solution(board, skill):
             delta[r][c] += delta[r-1][c]
 
     return sum(board[r][c] + delta[r][c] > 0 for r in range(R) for c in range(C))
+
+# 블록 이동하기
+# 로봇개발자 "무지"는 한 달 앞으로 다가온 "카카오배 로봇경진대회"에 출품할 로봇을 준비하고 있습니다.
+# 준비 중인 로봇은 2 x 1 크기의 로봇으로 "무지"는 "0"과 "1"로 이루어진 N x N 크기의 지도에서
+# 2 x 1 크기인 로봇을 움직여 (N, N) 위치까지 이동 할 수 있도록 프로그래밍을 하려고 합니다.
+# 로봇이 이동하는 지도는 가장 왼쪽, 상단의 좌표를 (1, 1)로 하며 지도 내에 표시된 숫자 "0"은 빈칸을 "1"은 벽을 나타냅니다.
+# 로봇은 벽이 있는 칸 또는 지도 밖으로는 이동할 수 없습니다.
+# 로봇은 처음에 아래 그림과 같이 좌표 (1, 1) 위치에서 가로방향으로 놓여있는 상태로 시작하며, 앞뒤 구분없이 움직일 수 있습니다.
+# 로봇이 움직일 때는 현재 놓여있는 상태를 유지하면서 이동합니다.
+# 위 그림과 같이 로봇은 90도씩 회전할 수 있습니다.
+# 단, 로봇이 차지하는 두 칸 중, 어느 칸이든 축이 될 수 있지만,
+# 회전하는 방향(축이 되는 칸으로부터 대각선 방향에 있는 칸)에는 벽이 없어야 합니다.
+# 로봇이 한 칸 이동하거나 90도 회전하는 데는 걸리는 시간은 정확히 1초 입니다.
+# "0"과 "1"로 이루어진 지도인 board가 주어질 때,
+# 로봇이 (N, N) 위치까지 이동하는데 필요한 최소 시간을 return 하도록 solution 함수를 완성해주세요.
+from collections import deque
+def can_move(cur1, cur2, new_board):
+    Y, X = 0, 1
+    cand = []
+    # 평행이동
+    DELTAS = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    for dy, dx in DELTAS:
+        nxt1 = (cur1[Y] + dy, cur1[X] + dx)
+        nxt2 = (cur2[Y] + dy, cur2[X] + dx)
+        if new_board[nxt1[Y]][nxt1[X]] == 0 and new_board[nxt2[Y]][nxt2[X]] == 0:
+            cand.append((nxt1, nxt2))
+    # 회전
+    if cur1[Y] == cur2[Y]: # 가로방향 일 때
+        UP, DOWN = -1, 1
+        for d in [UP, DOWN]:
+            if new_board[cur1[Y]+d][cur1[X]] == 0 and new_board[cur2[Y]+d][cur2[X]] == 0:
+                cand.append((cur1, (cur1[Y]+d, cur1[X])))
+                cand.append((cur2, (cur2[Y]+d, cur2[X])))
+    else: # 세로 방향 일 때
+        LEFT, RIGHT = -1, 1
+        for d in [LEFT, RIGHT]:
+            if new_board[cur1[Y]][cur1[X]+d] == 0 and new_board[cur2[Y]][cur2[X]+d] == 0:
+                cand.append(((cur1[Y], cur1[X]+d), cur1))
+                cand.append(((cur2[Y], cur2[X]+d), cur2))
+                
+    return cand
+
+def solution(board):
+    # board 외벽 둘러싸기
+    N = len(board)
+    new_board = [[1] * (N+2) for _ in range(N+2)]
+    for i in range(N):
+        for j in range(N):
+            new_board[i+1][j+1] = board[i][j]
+
+    # 현재 좌표 위치 큐 삽입, 확인용 set
+    que = deque([((1, 1), (1, 2), 0)])
+    confirm = set([((1, 1), (1, 2))])
+
+    while que:
+        cur1, cur2, count = que.popleft()
+        if cur1 == (N, N) or cur2 == (N, N):
+            return count
+        for nxt in can_move(cur1, cur2, new_board):
+            if nxt not in confirm:
+                que.append((*nxt, count+1))
+                confirm.add(nxt)
