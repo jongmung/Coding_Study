ff0de874df4a3f55b0d8cd05af67d2613100a055
jongmung <jmp99@naver.com>
2024-04-16 23:37:11 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -2063,3 +2063,48 @@ def solution(sizes):
             dp[s][e] = min(candidate)
             
     return dp[0][-1]
+
+# 아이템 줍기
+# 지형은 각 변이 x축, y축과 평행한 직사각형이 겹쳐진 형태로 표현하며,
+# 캐릭터는 이 다각형의 둘레(굵은 선)를 따라서 이동합니다.
+# 만약 직사각형을 겹친 후 다음과 같이 중앙에 빈 공간이 생기는 경우,
+# 다각형의 가장 바깥쪽 테두리가 캐릭터의 이동 경로가 됩니다.
+# 단, 서로 다른 두 직사각형의 x축 좌표 또는 y축 좌표가 같은 경우는 없습니다.
+# 즉, 위 그림처럼 서로 다른 두 직사각형이 꼭짓점에서 만나거나, 변이 겹치는 경우 등은 없습니다.
+# 다음 그림과 같이 지형이 2개 이상으로 분리된 경우도 없습니다.
+# 지형을 나타내는 직사각형이 담긴 2차원 배열 rectangle, 초기 캐릭터의 위치 characterX, characterY,
+# 아이템의 위치 itemX, itemY가 solution 함수의 매개변수로 주어질 때,
+# 캐릭터가 아이템을 줍기 위해 이동해야 하는 가장 짧은 거리를 return 하도록 solution 함수를 완성해주세요.
+from collections import deque
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    answer = 0
+    graph = [[-1 for _ in range(102)] for _ in range(102)]
+    visited = [[1 for _ in range(102)] for _ in range(102)]
+    direction = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    dq = deque()
+    for r in rectangle:
+        x1, y1, x2, y2 = map(lambda x: x*2, r)
+        for i in range(x1, x2+1):
+            for j in range(y1, y2+1):
+                # x1, x2, y1, y2는 테두리이므로 제외하고 내부만 0으로 채움
+                if x1 < i < x2 and y1 < j < y2:
+                    graph[i][j] = 0
+                # 다른 직사각형의 내부가 아니면서 테두리일 때 1로 채움
+                elif graph[i][j] != 0:
+                    graph[i][j] = 1                
+    # 반복문을 마치면 테두리는 1, 내부는 0, 외부는 -1이 될 것이다   
+    # 캐릭터와 아이템의 좌표도 2배씩 늘린다
+    cx, cy, ix, iy = 2*characterX, 2*characterY, 2*itemX, 2*itemY
+    dq.append((cx, cy))
+    while dq:
+        x, y = dq.popleft()
+        if x == ix and y == iy:
+        	# 답을 반환할 때 2로 나누어 반환해준다
+            answer = visited[x][y] // 2
+            break   
+        for k in range(4):
+            nx, ny = x + direction[k][0], y + direction[k][1]
+            if graph[nx][ny] == 1 and visited[nx][ny] == 1:
+                visited[nx][ny] += visited[x][y]
+                dq.append((nx, ny))
+    return answer
