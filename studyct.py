e9bfb397319a5205c4075755af844e5839456f8b
jongmung <jmp99@naver.com>
2024-04-21 23:59:06 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -2284,3 +2284,58 @@ def solution(n, k, camaand):
                     now = table[now].next
 
     return ''.join('O' if table[i].live else 'X' for i in range(n))
+
+# 길 찾기 게임
+# 그냥 지도를 주고 게임을 시작하면 재미가 덜해지므로,
+# 라이언은 방문할 곳의 2차원 좌표 값을 구하고 각 장소를 이진트리의 노드가 되도록 구성한 후,
+# 순회 방법을 힌트로 주어 각 팀이 스스로 경로를 찾도록 할 계획이다.
+# 라이언은 아래와 같은 특별한 규칙으로 트리 노드들을 구성한다.
+# 트리를 구성하는 모든 노드의 x, y 좌표 값은 정수이다.
+#   모든 노드는 서로 다른 x값을 가진다.
+#   같은 레벨(level)에 있는 노드는 같은 y 좌표를 가진다.
+#   자식 노드의 y 값은 항상 부모 노드보다 작다.
+#   임의의 노드 V의 왼쪽 서브 트리(left subtree)에 있는 모든 노드의 x값은 V의 x값보다 작다.
+#   임의의 노드 V의 오른쪽 서브 트리(right subtree)에 있는 모든 노드의 x값은 V의 x값보다 크다.
+# 곤경에 빠진 카카오 프렌즈를 위해 이진트리를 구성하는 노드들의 좌표가 담긴 배열 nodeinfo가 매개변수로 주어질 때,
+# 노드들로 구성된 이진트리를 전위 순회, 후위 순회한 결과를 2차원 배열에 순서대로 담아 return 하도록 solution 함수를 완성하자.
+import sys; sys.setrecursionlimit(10001)
+from collections import namedtuple
+Node = namedtuple('Node', ['x', 'y', 'id'])
+def solution(nodeinfo):
+    # 특정 노드를 기준으로 partition을 잘 하면 될 것 같습니다.
+    # 우선 nodeinfo를 (x, y, id)의 배열로 바꿉니다.
+    nodeinfo = [Node(x, y, id) for id, (x, y) in enumerate(nodeinfo, 1)]
+    
+    def preorder(arr):
+        if len(arr) < 1:
+            return [node.id for node in arr]
+        
+        # 전위순회부터 해보면, 배열에서 y좌표가 가장 큰 값을 pivot으로 두고
+        # 먼저 pivot의 id를 리턴 배열에 넣은 뒤
+        # x좌표의 대소를 기준으로 나뉜 두 배열에서
+        # 전위순회를 재귀적으로 돌려주면 됩니다.
+        pivot = max(arr, key=lambda node: node.y)
+        
+        res = [pivot.id]
+        res += preorder([node for node in arr if node.x < pivot.x])
+        res += preorder([node for node in arr if node.x > pivot.x])
+        
+        return res
+    
+    def postorder(arr):
+        if len(arr) < 1:
+            return [node.id for node in arr]
+        
+        # 후위순회는, 배열에서 y좌표가 가장 큰 값을 pivot으로 두고
+        # x좌표의 대소를 기준으로 나뉜 두 배열에서
+        # 후위순회를 재귀적으로 돌린 다음 pivot의 id를 리턴 배열에 넣으면 됩니다.
+        pivot = max(arr, key=lambda node: node.y)
+        
+        res = []
+        res += postorder([node for node in arr if node.x < pivot.x])
+        res += postorder([node for node in arr if node.x > pivot.x])
+        res.append(pivot.id)
+        
+        return res
+        
+    return preorder(nodeinfo), postorder(nodeinfo)
