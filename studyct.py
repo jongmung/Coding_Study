c3b0a111c82d93959088bfddab16caf81a924d9a
jongmung <jmp99@naver.com>
2024-04-22 16:23:38 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -2339,3 +2339,39 @@ def solution(nodeinfo):
         return res
         
     return preorder(nodeinfo), postorder(nodeinfo)
+
+# 추석 트래픽
+# 장애 대비용 서버 증설 여부를 결정하기 위해 작년 추석 기간인 9월 15일 로그 데이터를 분석한 후 초당 최대 처리량을 계산해보기로 했다.
+# 초당 최대 처리량은 요청의 응답 완료 여부에 관계없이 임의 시간부터 1초(=1,000밀리초)간 처리하는 요청의 최대 개수를 의미한다.
+#   solution 함수에 전달되는 lines 배열은 N(1 ≦ N ≦ 2,000)개의 로그 문자열로 되어 있으며,
+#       각 로그 문자열마다 요청에 대한 응답완료시간 S와 처리시간 T가 공백으로 구분되어 있다.
+#   응답완료시간 S는 작년 추석인 2016년 9월 15일만 포함하여 고정 길이 2016-09-15 hh:mm:ss.sss 형식으로 되어 있다.
+#   처리시간 T는 0.1s, 0.312s, 2s 와 같이 최대 소수점 셋째 자리까지 기록하며 뒤에는 초 단위를 의미하는 s로 끝난다.
+#       예를 들어, 로그 문자열 2016-09-15 03:10:33.020 0.011s은
+#       "2016년 9월 15일 오전 3시 10분 33.010초"부터 "2016년 9월 15일 오전 3시 10분 33.020초"까지 "0.011초" 동안 처리된 요청을 의미한다.
+#       (처리시간은 시작시간과 끝시간을 포함)
+#   서버에는 타임아웃이 3초로 적용되어 있기 때문에 처리시간은 0.001 ≦ T ≦ 3.000이다.
+#   lines 배열은 응답완료시간 S를 기준으로 오름차순 정렬되어 있다.
+def solution(lines):
+    answer = 0
+    log = [] # log의 (시작시간, 끝시간) 저장
+    for line in lines:
+        date, s, t = line.split() # 날짜, 응답완료시간, 처리시간
+        s = s.split(':')
+        t = t.replace('s', '')
+        end = (int(s[0])*3600 + int(s[1])*60 + float(s[2])) * 1000 # 끝시간을 msec 단위로 저장
+        start = end - float(t)*1000 + 1 # 시작 시간을 msec 단위로 저장
+        log.append([start, end])
+    for x in log:
+    	# 최대 초당 처리량 구하기
+        answer = max(answer, throughput(log, x[0], x[0] + 1000), throughput(log, x[1], x[1] + 1000))
+
+    return answer
+
+# 초당 처리량을 구하는 함수
+def throughput(log, start, end):
+    cnt = 0
+    for x in log:
+        if x[0] < end and x[1] >= start:
+            cnt += 1
+    return cnt
