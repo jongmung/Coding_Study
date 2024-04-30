e8e277e93ef3fa807fb6e517976bf0f984b7a1a4
jongmung <jmp99@naver.com>
2024-04-30 23:29:08 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -2882,3 +2882,21 @@ def solution(board, aloc, bloc):
     global n,m
     n, m = len(board), len(board[0])
     return play(board,aloc[0],aloc[1],bloc[0],bloc[1])
+
+# 도둑질
+# 도둑이 어느 마을을 털 계획을 하고 있습니다. 이 마을의 모든 집들은 아래 그림과 같이 동그랗게 배치되어 있습니다.
+# 각 집들은 서로 인접한 집들과 방범장치가 연결되어 있기 때문에 인접한 두 집을 털면 경보가 울립니다.
+# 각 집에 있는 돈이 담긴 배열 money가 주어질 때, 도둑이 훔칠 수 있는 돈의 최댓값을 return 하도록 solution 함수를 작성하세요.
+def solution(money):
+    dp1 = [0] * len(money)
+    dp2 = [0] * len(money)
+    # 1번 집을 터는 경우
+    dp1[0] = money[0]
+    for i in range(1, len(money) - 1):  # 마지막 집은 털지 못함
+        dp1[i] = max(dp1[i - 1], dp1[i - 2] + money[i])
+    # 1번 집을 안터는 경우
+    dp1[0] = 0
+    for i in range(1, len(money)):
+        dp2[i] = max(dp2[i - 1], dp2[i - 2] + money[i])
+
+    return max(dp1[-2], dp2[-1])
