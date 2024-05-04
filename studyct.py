5e2142ec345c3b352ce012e496e18bf9d12a126e
jongmung <jmp99@naver.com>
2024-05-04 23:57:43 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -2959,3 +2959,43 @@ def solution(n):
 
     answer = dp[n]
     return answer
+
+# 징검다리
+# 출발지점부터 distance만큼 떨어진 곳에 도착지점이 있습니다.
+# 그리고 그사이에는 바위들이 놓여있습니다. 바위 중 몇 개를 제거하려고 합니다.
+# 출발지점부터 도착지점까지의 거리 distance, 바위들이 있는 위치를 담은 배열 rocks, 제거할 바위의 수 n이 매개변수로 주어질 때,
+# 바위를 n개 제거한 뒤 각 지점 사이의 거리의 최솟값 중에 가장 큰 값을 return 하도록 solution 함수를 작성해주세요.
+def solution(distance, rocks, n):
+	# 커트라인 최솟값 = 1
+    left = 1
+    # 커트라인 최댓값 = distance
+    right = distance
+    # 바위 위치를 정렬하고, 도착지점 append
+    rocks.sort()
+    rocks.append(distance)
+    while left <= right:
+        mid = (left+right)//2
+        # 제거한 바위 개수
+        delete = 0
+        # 이전 바위의 위치
+        prev_rock = 0
+        for rock in rocks:
+            dist = rock - prev_rock
+            # 거리가 커트라인 보다 작다면, 바위를 제거
+            if dist < mid:
+                delete += 1
+                # 제거한 바위가 너무 많다면 break
+                if delete > n:
+                    break
+            # 바위를 제거하지 않았다면, prev_rock을 갱신
+            else:
+                prev_rock = rock
+                
+        # 초과 제거한 경우 : 커트라인이 너무 높음
+        if delete > n:
+            right = mid -1
+        # 이하 제거한 경우 : 커트라인이 적절하거나 너무 낮음
+        else:
+            answer = mid
+            left = mid + 1
+    return answer
