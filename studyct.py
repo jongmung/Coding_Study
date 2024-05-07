034f53dc8e41217fbb5669a75a6e1840c0208254
jongmung <jmp99@naver.com>
2024-05-07 23:29:47 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -3058,3 +3058,36 @@ def solution(words):
 
     # 단어마다 입력해야 하는 문자 수를 합해서 반환
     return sum(result)
+
+# 쿠키 구입
+# 과자를 바구니 단위로 파는 가게가 있습니다.
+# 이 가게는 1번부터 N번까지 차례로 번호가 붙은 바구니 N개가 일렬로 나열해 놨습니다.
+# 철수는 두 아들에게 줄 과자를 사려합니다. 첫째 아들에게는 l번 바구니부터 m번 바구니까지,
+# 둘째 아들에게는 m+1번 바구니부터 r번 바구니까지를 주려합니다.
+# 단, 두 아들이 받을 과자 수는 같아야 합니다(1 <= l <= m, m+1 <= r <= N).
+# 즉, A[i] 를 i번 바구니에 들어있는 과자 수라고 했을 때, A[l]+..+A[m] = A[m+1]+..+A[r] 를 만족해야 합니다.
+# 각 바구니 안에 들은 과자 수가 차례로 들은 배열 cookie가 주어질 때,
+# 조건에 맞게 과자를 살 경우 한 명의 아들에게 줄 수 있는 가장 많은 과자 수를 return 하는 solution 함수를 완성해주세요.
+# (단, 조건에 맞게 과자를 구매할 수 없다면 0을 return 합니다)
+def solution(cookie):
+    answer = 0
+    n = len(cookie)
+    for i in range(n - 1):
+        left_sum, left_idx = cookie[i], i
+        right_sum, right_idx = cookie[i + 1], i + 1
+
+        while True:
+            if left_sum == right_sum:
+                answer = max(answer, left_sum)
+                # answer = max(answer, right_sum)
+                
+            if left_idx > 0 and left_sum <= right_sum:
+                left_idx -= 1
+                left_sum += cookie[left_idx]
+            elif right_idx < n - 1 and right_sum <= left_sum:
+                right_idx += 1
+                right_sum += cookie[right_idx]
+            else:
+                break
+
+    return answer
