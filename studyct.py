d59a464718d77263f52a7accbec69ca9ae03c73a
jongmung <jmp99@naver.com>
2024-05-08 21:30:51 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -3091,3 +3091,45 @@ def solution(cookie):
                 break
 
     return answer
+
+# 등대
+# 인천 앞바다에는 1부터 n까지 서로 다른 번호가 매겨진 등대 n개가 존재합니다.
+# 등대와 등대 사이를 오가는 뱃길이 n-1개 존재하여, 어느 등대에서 출발해도 다른 모든 등대까지 이동할 수 있습니다.
+# 등대 관리자 윤성이는 전력을 아끼기 위하여, 이 중 몇 개의 등대만 켜 두려고 합니다.
+# 하지만 등대를 아무렇게나 꺼버리면, 뱃길을 오가는 배들이 위험할 수 있습니다.
+# 한 뱃길의 양쪽 끝 등대 중 적어도 하나는 켜져 있도록 등대를 켜 두어야 합니다.
+# 등대의 개수 n과 각 뱃길이 연결된 등대의 번호를 담은 이차원 배열 lighthouse가 매개변수로 주어집니다.
+# 윤성이가 켜 두어야 하는 등대 개수의 최솟값을 return 하도록 solution 함수를 작성해주세요.
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(1000001)
+A = defaultdict(list)
+vis = [False] * 1000001
+
+# 자신을 포함한 subtree에서, 내가 켜졌을 때의 최소 점등 등대 개수와
+# 내가 꺼졌을 때의 최소 점등 등대 개수를 반환합니다.
+def dfs(u):
+    vis[u] = True
+    if not A[u]:
+        # u가 leaf라면 내가 켜졌을 떄의 최소 점등 등대 개수는 1
+        # 내가 꺼졌을 때의 최소 점등 등대 개수는 0
+        return 1, 0
+    
+    # u가 leaf가 아니라면
+    on, off = 1, 0
+    for v in [v for v in A[u] if not vis[v]]:
+        # 내가 켜졌다면 child들은 켜지든 꺼지든 상관 없습니다. -> 킨 것과 끈 것중 최소값을 취함
+        # 내가 꺼졌다면 child들은 무조건 켜져야 합니다.
+        # 이 점을 생각해서 leaf들의 정보를 취합, 정리합니다.
+        child_on, child_off = dfs(v)
+        on += min(child_on, child_off)
+        off += child_on
+    return on, off
+
+def solution(n, lighthouse):
+    for u, v in lighthouse:
+        A[u].append(v)
+        A[v].append(u)
+        
+    on, off = dfs(1)
+    return min(on, off)
