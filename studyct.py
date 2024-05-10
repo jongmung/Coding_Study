39b325bb0f9c0934a2b132f02dcfda789df9be2e
jongmung <jmp99@naver.com>
2024-05-10 23:52:55 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -3178,3 +3178,48 @@ def solution(h1, m1, s1, h2, m2, s2):
         startTime += 1
     
     return answer
+
+# 주사위 고르기
+# A와 B가 n개의 주사위를 가지고 승부를 합니다. 주사위의 6개 면에 각각 하나의 수가 쓰여 있으며,
+# 주사위를 던졌을 때 각 면이 나올 확률은 동일합니다.
+# 각 주사위는 1 ~ n의 번호를 가지고 있으며, 주사위에 쓰인 수의 구성은 모두 다릅니다.
+# A가 먼저 n / 2개의 주사위를 가져가면 B가 남은 n / 2개의 주사위를 가져갑니다.
+# 각각 가져간 주사위를 모두 굴린 뒤, 나온 수들을 모두 합해 점수를 계산합니다.
+# 점수가 더 큰 쪽이 승리하며, 점수가 같다면 무승부입니다.
+# A는 자신이 승리할 확률이 가장 높아지도록 주사위를 가져가려 합니다.
+# 주사위에 쓰인 수의 구성을 담은 2차원 정수 배열 dice가 매개변수로 주어집니다.
+# 이때, 자신이 승리할 확률이 가장 높아지기 위해
+# A가 골라야 하는 주사위 번호를 오름차순으로 1차원 정수 배열에 담아 return 하도록 solution 함수를 완성해 주세요.
+# 승리할 확률이 가장 높은 주사위 조합이 유일한 경우만 주어집니다.
+from itertools import combinations, product
+def solution(dice):
+    n = len(dice)
+    A, result = [], []
+    cases = list(combinations(range(n), n//2)) # 뽑을 수 있는 경우의 수 (인덱스)
+    for case in cases:
+        dices = [dice[c] for c in case] # 인덱스에 해당하는 실제 주사위
+        nums = sorted([sum(i) for i in product(*dices)]) # 뽑은 주사위의 합의 경우의 수
+        A.append(nums)
+        
+    a, p = 0, len(A)
+    for i in range(p):
+        B = A[p-i-1] # B와 A는대칭의 구조를 가지므로 B = A[p-i-1]
+        
+        #각 조합이 이기는 횟수 이분탐색
+        temp = 0
+        for t in A[i]:
+            left, right = 0, len(B)-1
+            while left <= right:
+                mid = (left + right) // 2
+                if B[mid] < t:
+                    left = mid + 1
+                else :
+                    right = mid - 1
+            temp += left
+        
+        # 가장 승리 확률이 높은 경우의 수 반환
+        if a < temp:
+            a = temp
+            result = [x + 1 for x in cases[i]]
+    
+    return result
