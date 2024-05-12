d3fb103b4701ba1391190261a32f8894d0853b10
jongmung <jmp99@naver.com>
2024-05-12 23:20:48 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -3223,3 +3223,44 @@ def solution(dice):
             result = [x + 1 for x in cases[i]]
     
     return result
+
+# 지형이동
+# N x N 크기인 정사각 격자 형태의 지형이 있습니다. 각 격자 칸은 1 x 1 크기이며,
+# 숫자가 하나씩 적혀있습니다. 격자 칸에 적힌 숫자는 그 칸의 높이를 나타냅니다.
+# 이 지형의 아무 칸에서나 출발해 모든 칸을 방문하는 탐험을 떠나려 합니다. 
+# 칸을 이동할 때는 상, 하, 좌, 우로 한 칸씩 이동할 수 있는데, 현재 칸과 이동하려는 칸의 높이 차가 height 이하여야 합니다.
+# 높이 차가 height 보다 많이 나는 경우에는 사다리를 설치해서 이동할 수 있습니다. 이때, 사다리를 설치하는데 두 격자 칸의 높이차만큼 비용이 듭니다.
+# 따라서, 최대한 적은 비용이 들도록 사다리를 설치해서 모든 칸으로 이동 가능하도록 해야 합니다.
+# 설치할 수 있는 사다리 개수에 제한은 없으며, 설치한 사다리는 철거하지 않습니다.
+# 각 격자칸의 높이가 담긴 2차원 배열 land와 이동 가능한 최대 높이차 height가 매개변수로 주어질 때,
+# 모든 칸을 방문하기 위해 필요한 사다리 설치 비용의 최솟값을 return 하도록 solution 함수를 완성해주세요.
+import heapq
+def solution(land, height):
+    dx=[1,-1,0,0]
+    dy=[0,0,1,-1]
+    n=len(land)
+    visited=[[False]*n for x in range(n)]
+    #시작점은 0,0 말고 아무거나 해도 됨
+    h=[(0,0,0)]
+    res=0
+    while h:
+        cnt,x,y=heapq.heappop(h) 
+        
+        if visited[x][y]:
+            continue
+            
+        res+=cnt        
+        visited[x][y]=True
+        for i in range(4):
+            xx=dx[i]+x
+            yy=dy[i]+y
+            #방문하지 않은 경우만 check
+            if 0<=xx<n and 0<=yy<n and not visited[xx][yy]:
+                temp=abs(land[x][y]-land[xx][yy])
+                #height보다 차이가 크다면 그 차이를 넣어줘야함(사다리 건설비용)
+                #height보다 작다면 사다리 건설비용이 들지 않기 때문에 0을 삽입
+                if temp>height:
+                    heapq.heappush(h,(temp,xx,yy))
+                else:
+                    heapq.heappush(h,(0,xx,yy))
+    return res
