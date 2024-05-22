d2eb01ed7df61ce3e15eb2785a5bac22d4180340
jongmung <jmp99@naver.com>
2024-05-22 23:49:07 +0900

    studyctt

--- studyct.py
+++ studyct.py
@@ -3690,3 +3690,48 @@ def solution(k, n, reqs):
         if wait_time < answer:
             answer = wait_time
     return answer
+
+# 에어컨
+# 현대모비스에서 개발한 실내공조 제어 시스템은 차내에 승객이 탑승 중일 때
+# 항상 쾌적한 실내온도(t1 ~ t21)를 유지할 수 있도록 합니다. 현재(0분) 실내온도는 실외온도와 같습니다.
+# 실내공조 제어 시스템은 실내온도를 조절하기 위해 에어컨의 전원을 켜 희망온도를 설정합니다.
+# 희망온도는 에어컨의 전원이 켜져 있는 동안 원하는 값으로 변경할 수 있습니다.
+# 실내온도와 희망온도가 다르다면 1분 뒤 실내온도가 희망온도와 같아지는 방향으로 1도 상승 또는 하강합니다.
+# 실내온도가 희망온도와 같다면 에어컨이 켜져 있는 동안은 실내온도가 변하지 않습니다.
+# 에어컨의 전원을 끄면 실내온도가 실외온도와 같아지는 방향으로 매 분 1도 상승 또는 하강합니다.
+# 실내온도와 실외온도가 같다면 실내온도는 변하지 않습니다.
+# 에어컨의 소비전력은 현재 실내온도에 따라 달라집니다.
+# 에어컨의 희망온도와 실내온도가 다르다면 매 분 전력을 a만큼 소비하고,
+# 희망온도와 실내온도가 같다면 매 분 전력을 b만큼 소비합니다. 에어컨이 꺼져 있다면 전력을 소비하지 않으며,
+# 에어컨을 켜고 끄는데 필요한 시간과 전력은 0이라고 가정합니다.
+# 실내공조 제어 시스템은 차내에 승객이 탑승 중일 때 실내온도를 t1 ~ t2도 사이로 유지하면서, 에어컨의 소비전력을 최소화합니다.
+# 실외온도를 나타내는 정수 temperature, 쾌적한 실내온도의 범위를 나타내는 정수 t1, t2,
+# 에어컨의 소비전력을 나타내는 정수 a, b와 승객이 탑승 중인 시간을 나타내는 1차원 정수 배열 onboard가 매개변수로 주어집니다.
+# 승객이 탑승 중인 시간에 쾌적한 실내온도를 유지하기 위한 최소 소비전력을 return 하도록 solution 함수를 완성해 주세요.
+import math
+def solution(temperature, t1, t2, a, b, onboard):
+    # 0으로 정규화
+    diff = temperature - t2 if temperature > t2 else t1 - temperature
+    # 시간0에 대해 초기 비용으로 초기화
+    result = [[ 1000000 for i in range(diff+1) ]+[0]]
+    last_passenger_index = 0
+    for i in range(1, len(onboard)):
+        arr = []
+        for j in range(diff+2):
+            temp = [result[i-1][j]+b]
+            # 할수있는 행동(비용)은 온도유지(b), 온도 내리기(a), 온도올리기(0)
+            if j == diff + 1:
+                temp.append(result[i-1][j])
+            if j <= diff:
+                temp.append(result[i-1][j+1] + a)
+            if j > 0:
+                temp.append(result[i-1][j-1])
+            min_cost = min(temp)
+            # 시간i에 대해 승객에 탑승 중이고, 적정온도 범위 밖이라면.
+            if onboard[i] == 1:
+                last_passenger_index = i
+                if j > 1:
+                    min_cost = 100000 # 100 * 1000
+            arr.append(min_cost)
+        result.append(arr)
+    return min(result[last_passenger_index])
